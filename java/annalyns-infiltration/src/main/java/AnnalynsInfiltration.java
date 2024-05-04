f33660bca2037e49a76780741e1843e4bb8a1e9b
Irfan <irfansalim77@gmail.com>
2024-05-05 00:17:07 +0530

    Add  M java/annalyns-infiltration/.gradle/8.7/executionHistory/executionHistory.bin  M java/annalyns-infiltration/.gradle/8.7/executionHistory/executionHistory.lock  M java/annalyns-infiltration/.gradle/8.7/fileHashes/fileHashes.bin  M java/annalyns-infiltration/.gradle/8.7/fileHashes/fileHashes.lock  M java/annalyns-infiltration/.gradle/8.7/fileHashes/resourceHashesCache.bin  M java/annalyns-infiltration/.gradle/buildOutputCleanup/buildOutputCleanup.lock  M java/annalyns-infiltration/build/classes/java/main/AnnalynsInfiltration.class  M java/annalyns-infiltration/build/reports/tests/test/classes/AnnalynsInfiltrationTest.html  M java/annalyns-infiltration/build/reports/tests/test/index.html  M java/annalyns-infiltration/build/reports/tests/test/packages/default-package.html  M java/annalyns-infiltration/build/test-results/test/TEST-AnnalynsInfiltrationTest.xml  M java/annalyns-infiltration/build/test-results/test/binary/results.bin  M java/annalyns-infiltration/build/tmp/compileJava/compileTransaction/stash-dir/AnnalynsInfiltration.class.uniqueId0  M java/annalyns-infiltration/build/tmp/compileJava/previous-compilation-data.bin  M java/annalyns-infiltration/build/tmp/compileTestJava/previous-compilation-data.bin  M java/annalyns-infiltration/src/main/java/AnnalynsInfiltration.java ?? java/annalyns-infiltration/build/tmp/compileTestJava/compileTransaction/

--- java/annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
+++ java/annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
@@ -5,6 +5,7 @@ class AnnalynsInfiltration {
 
     public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
       if (knightIsAwake || archerIsAwake) return true;
+      else if (!knightIsAwake && !archerIsAwake && prisonerIsAwake) return true;
       else return false;
     }
 
@@ -14,7 +15,7 @@ class AnnalynsInfiltration {
     }
 
     public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake, boolean petDogIsPresent) {
-      if (!archerIsAwake && prisonerIsAwake && petDogIsPresent) return true;
+      if (!archerIsAwake &&  petDogIsPresent) return true;
       else if (!archerIsAwake && !knightIsAwake && prisonerIsAwake && !petDogIsPresent) return true;
       else return false;
     }
