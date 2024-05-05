8c4f00dd6cdec37fb82822d08337e759471b5409
Irfan <irfansalim77@gmail.com>
2024-05-05 13:02:28 +0530

    Add  M java/log-levels/gradlew  M java/log-levels/src/main/java/LogLevels.java ?? java/log-levels/.gradle/ ?? java/log-levels/build/

--- java/log-levels/src/main/java/LogLevels.java
+++ java/log-levels/src/main/java/LogLevels.java
@@ -1,14 +1,20 @@
 public class LogLevels {
     
     public static String message(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLevels.message() method");
+      String[] arr = logLine.split(":");
+      String newLogLine = arr[1].trim();
+      return newLogLine;
     }
 
     public static String logLevel(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLevels.logLevel() method");
+      String[] arr = logLine.split(":");
+      String level = arr[0].substring(1, arr[0].length() - 1).trim().toLowerCase();
+      return level;
     }
 
     public static String reformat(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLevels.reformat() method");
+      String message = message(logLine);
+      String level = logLevel(logLine);
+      return message + " (" + level + ")";
     }
 }
