3be501e46ac655986787e29260c39cd26b7eac1e
Irfan <irfansalim77@gmail.com>
2024-05-06 10:11:27 +0530

    Add  M java/log-levels/src/main/java/LogLevels.java

--- java/log-levels/src/main/java/LogLevels.java
+++ java/log-levels/src/main/java/LogLevels.java
@@ -18,3 +18,51 @@ public class LogLevels {
       return message + " (" + level + ")";
     }
 }
+
+import static java.lang.String.format;
+
+public class LogLevels {
+
+    public static String message(String logLine) {
+        return logLine.split("]: ")[1]
+                .trim();
+    }
+
+    public static String logLevel(String logLine) {
+        return logLine.split("]: ")[0]
+                .substring(1)
+                .toLowerCase();
+    }
+
+    public static String reformat(String logLine) {
+        return format("%s (%s)", message(logLine), logLevel(logLine));
+    }
+}
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class LogLevels {
+
+    private static final String REGEX = "\\[(?<level>[A-Z]+)\\]:\\s(?<message>.+)";
+    private static final Pattern PATTERN = Pattern.compile(REGEX);
+    
+    public static String message(String logLine) {
+        return match(logLine).group("message").strip();
+    }
+
+    public static String logLevel(String logLine) {
+        return match(logLine).group("level").toLowerCase();
+    }
+
+    public static String reformat(String logLine) {
+        return String.format("%s (%s)", 
+            message(logLine), logLevel(logLine));
+    }
+
+    private static Matcher match(String logLine) {
+        var matcher = PATTERN.matcher(logLine);
+        matcher.find();
+        return matcher;
+    }
+}
