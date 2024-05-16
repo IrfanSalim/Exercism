6fa22fed45322c5aea938d632ec1a56ac5c13996
Irfan <irfansalim77@gmail.com>
2024-05-17 00:25:33 +0530

    Add  M java/squeaky-clean/.gradle/8.7/executionHistory/executionHistory.bin  M java/squeaky-clean/.gradle/8.7/executionHistory/executionHistory.lock  M java/squeaky-clean/.gradle/8.7/fileHashes/fileHashes.bin  M java/squeaky-clean/.gradle/8.7/fileHashes/fileHashes.lock  M java/squeaky-clean/.gradle/8.7/fileHashes/resourceHashesCache.bin  M java/squeaky-clean/.gradle/buildOutputCleanup/buildOutputCleanup.lock  M java/squeaky-clean/build/classes/java/main/SqueakyClean.class  M java/squeaky-clean/build/reports/tests/test/classes/SqueakyCleanTest.html  M java/squeaky-clean/build/reports/tests/test/index.html  M java/squeaky-clean/build/reports/tests/test/packages/default-package.html  M java/squeaky-clean/build/test-results/test/TEST-SqueakyCleanTest.xml  M java/squeaky-clean/build/test-results/test/binary/results.bin  M java/squeaky-clean/build/tmp/compileJava/compileTransaction/stash-dir/SqueakyClean.class.uniqueId0  M java/squeaky-clean/build/tmp/compileJava/previous-compilation-data.bin  M java/squeaky-clean/build/tmp/compileTestJava/previous-compilation-data.bin  M java/squeaky-clean/src/main/java/SqueakyClean.java ?? java/squeaky-clean/build/tmp/compileTestJava/compileTransaction/

--- java/squeaky-clean/src/main/java/SqueakyClean.java
+++ java/squeaky-clean/src/main/java/SqueakyClean.java
@@ -1,9 +1,35 @@
+import java.util.HashMap;
+
 class SqueakyClean {
     static String clean(String identifier) {
-      String clean = identifier.replaceAll("[^a-zA-Z0-9]", "_");
-      if (clean.length() == 0) {
-        return "a";
+      int maxLength = identifier.length();
+      String regex = "[a-zA-Z0-9_ -]+";
+      HashMap<Character, Character> hm = new HashMap<>();
+      hm.put('3', 'e');
+      hm.put('4', 'a');
+      hm.put('1', 'l');
+      hm.put('0', 'o');
+      hm.put('7', 't');
+      StringBuilder sb = new StringBuilder(maxLength);
+      boolean camelCase = false;
+      for (char c : identifier.toCharArray()) {
+        if (String.valueOf(c).matches(regex)) {
+          if (Character.isWhitespace(c)) {
+            sb.append("_");
+          } else if (c == '-') {
+            camelCase = true;
+          } else if (hm.containsKey(c)) {
+            sb.append(hm.get(c));
+          } else {
+            if (camelCase) {
+              sb.append(Character.toUpperCase(c));
+              camelCase = false;
+            }
+            else sb.append(c);
+          }
+        }
       }
-      return clean;
+      
+      return sb.toString();
     }
 }
