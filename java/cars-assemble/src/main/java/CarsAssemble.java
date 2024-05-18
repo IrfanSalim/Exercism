caa319ffb6a137fb6a4e16900baf264cfd1d1107
Irfan <irfansalim77@gmail.com>
2024-05-18 20:27:04 +0530

    Add  M java/cars-assemble/gradlew  M java/cars-assemble/src/main/java/CarsAssemble.java ?? java/cars-assemble/.gradle/ ?? java/cars-assemble/build/

--- java/cars-assemble/src/main/java/CarsAssemble.java
+++ java/cars-assemble/src/main/java/CarsAssemble.java
@@ -1,10 +1,25 @@
 public class CarsAssemble {
 
     public double productionRatePerHour(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.productionRatePerHour() method");
+      int success = successRate(speed);
+      return success / 100 * speed * 221;
     }
 
     public int workingItemsPerMinute(int speed) {
         throw new UnsupportedOperationException("Please implement the CarsAssemble.workingItemsPerMinute() method");
+    }
+
+    private int successRate(int speed) {
+      if (speed > 0 && speed < 5) {
+        return 100;
+      } else if (speed >= 5 && speed < 9) {
+        return 90;
+      } else if (speed == 9) {
+        return 80;
+      } else if (speed == 10) {
+        return 77;
+      } else {
+        return 0;
+      }
     }
 }
