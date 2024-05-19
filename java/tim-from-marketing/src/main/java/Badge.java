47610ad5958899705309b2b19ae80de4f71327eb
Irfan <irfansalim77@gmail.com>
2024-05-19 21:26:05 +0530

    Add  M java/tim-from-marketing/.gradle/8.7/executionHistory/executionHistory.lock  M java/tim-from-marketing/.gradle/buildOutputCleanup/buildOutputCleanup.lock  M java/tim-from-marketing/src/main/java/Badge.java

--- java/tim-from-marketing/src/main/java/Badge.java
+++ java/tim-from-marketing/src/main/java/Badge.java
@@ -5,3 +5,20 @@ class Badge {
       return String.format("%s%s - %s", idStr, name, departmentStr);
     }
 }
+
+
+class Badge {
+    public String print(Integer id, String name, String department) {
+        return ((id == null) ? "" : "["+id+"] - ")+name+" - "+((department == null) ? "OWNER" : department.toUpperCase());
+    }
+}
+
+
+class Badge {
+    public String print(Integer id, String name, String department) {
+        return String.format("%s%s - %s",
+                id != null ? String.format("[%d] - ", id) : "",
+                name,
+                department != null ? department.toUpperCase() : "OWNER");
+    }
+}
