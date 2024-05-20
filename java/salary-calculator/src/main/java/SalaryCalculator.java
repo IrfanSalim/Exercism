b9199a5f74cb74f26608ca3ef0bd155d54ad91a6
Irfan <irfansalim77@gmail.com>
2024-05-20 19:01:50 +0530

    Add  M java/salary-calculator/gradlew  M java/salary-calculator/src/main/java/SalaryCalculator.java ?? java/salary-calculator/.gradle/ ?? java/salary-calculator/build/

--- java/salary-calculator/src/main/java/SalaryCalculator.java
+++ java/salary-calculator/src/main/java/SalaryCalculator.java
@@ -1,17 +1,18 @@
 public class SalaryCalculator {
     public double salaryMultiplier(int daysSkipped) {
-        throw new UnsupportedOperationException("Please implement the SalaryCalculator.salaryMultiplier() method");
+      return daysSkipped < 5 ? 1.0 : 0.85;
     }
 
     public int bonusMultiplier(int productsSold) {
-        throw new UnsupportedOperationException("Please implement the SalaryCalculator.bonusMultiplier() method");
+      return productsSold < 20 ? 10 : 13;
     }
 
     public double bonusForProductsSold(int productsSold) {
-        throw new UnsupportedOperationException("Please implement the SalaryCalculator.bonusForProductsSold() method");
+      return productsSold * bonusMultiplier(productsSold);
     }
 
     public double finalSalary(int daysSkipped, int productsSold) {
-        throw new UnsupportedOperationException("Please implement the SalaryCalculator.finalSalary() method");
+      double salary = salaryMultiplier(daysSkipped) * 1000.0 + bonusForProductsSold(productsSold);
+      return salary < 2000.0 ? salary : 2000.0;
     } 
 }
