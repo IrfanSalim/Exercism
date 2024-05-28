941863c007c181c4a4a89fb7c27b6385ba38fd31
Irfan <irfansalim77@gmail.com>
2024-05-28 22:12:11 +0530

    Add  M java/grains/gradlew  M java/grains/src/main/java/Grains.java  M java/grains/src/test/java/GrainsTest.java ?? java/grains/.gradle/ ?? java/grains/build/

--- java/grains/src/main/java/Grains.java
+++ java/grains/src/main/java/Grains.java
@@ -1,13 +1,42 @@
 import java.math.BigInteger;
 
 class Grains {
+    BigInteger[] squares = new BigInteger[64];
+    BigInteger sum = BigInteger.ZERO;
+
+    public Grains() {
+        BigInteger count = BigInteger.ONE;
+        for (int i = 0; i < 64; i++) {
+            squares[i] = count;
+            sum = sum.add(count);
+            count = count.multiply(BigInteger.TWO);
+        }
+    }
 
     BigInteger grainsOnSquare(final int square) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (square < 1 || square > 64) {
+          throw new IllegalArgumentException("square must be between 1 and 64");
+        }
+        return squares[square - 1];
     }
 
     BigInteger grainsOnBoard() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return sum;
+    }
+}
+
+import java.math.BigInteger;
+
+class Grains {
+
+    BigInteger grainsOnSquare(final int square) {
+        if ((square <= 0) || (square >= 65)) {
+            throw new IllegalArgumentException("square must be between 1 and 64");
+        }
+        return BigInteger.valueOf(2).pow(square - 1);
     }
 
+    BigInteger grainsOnBoard() {
+        return BigInteger.valueOf(2).pow(64).subtract(BigInteger.ONE);
+    }
 }
