7beb60fe60996c1e6021a799a4e2015909fedc77
Irfan <irfansalim77@gmail.com>
2024-06-02 23:13:42 +0530

    Add  M java/resistor-color-trio/gradlew  M java/resistor-color-trio/src/main/java/ResistorColorTrio.java ?? java/resistor-color-trio/.gradle/ ?? java/resistor-color-trio/build/

--- java/resistor-color-trio/src/main/java/ResistorColorTrio.java
+++ java/resistor-color-trio/src/main/java/ResistorColorTrio.java
@@ -1,5 +1,50 @@
+import java.util.HashMap;
+
 class ResistorColorTrio {
+    // Method to initialize and return the color codes map
+    private HashMap<String, String> getColorCodes() {
+        HashMap<String, String> colorCodes = new HashMap<>();
+        colorCodes.put("black", "0");
+        colorCodes.put("brown", "1");
+        colorCodes.put("red", "2");
+        colorCodes.put("orange", "3");
+        colorCodes.put("yellow", "4");
+        colorCodes.put("green", "5");
+        colorCodes.put("blue", "6");
+        colorCodes.put("violet", "7");
+        colorCodes.put("grey", "8");
+        colorCodes.put("white", "9");
+        return colorCodes;
+    }
+
+    private String getUnit(String color) {
+      int number = Integer.parseInt(color);
+      switch (number) {
+        case 9:
+          return " gigaohms";
+        case 8:
+          return "00 megaohms";
+        case 7:
+          return "0 megaohms";
+        case 6:
+          return " megaohms";
+        case 5:
+          return "00 kiloohms";
+        case 4:
+          return "0 kiloohms";
+        case 3:
+          return " kiloohms";
+        case 2:
+          return "00 ohms";
+        case 1:
+          return "0 ohms";
+      }
+      return " ohms";
+    }
+
     String label(String[] colors) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+      HashMap<String, String> colorCodes = getColorCodes();
+      String code = colorCodes.get(colors[0]) + colorCodes.get(colors[1]) + getUnit(colorCodes.get(colors[2]));
+      return code;
     }
 }
