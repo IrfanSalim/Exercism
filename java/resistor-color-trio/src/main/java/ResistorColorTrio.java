65c8dd7d0d9c5a0392d9b7948b3f64b72e636394
Irfan <irfansalim77@gmail.com>
2024-06-02 23:53:10 +0530

    Add  M java/resistor-color-trio/.gradle/8.7/executionHistory/executionHistory.bin  M java/resistor-color-trio/.gradle/8.7/executionHistory/executionHistory.lock  M java/resistor-color-trio/.gradle/8.7/fileHashes/fileHashes.bin  M java/resistor-color-trio/.gradle/8.7/fileHashes/fileHashes.lock  M java/resistor-color-trio/.gradle/8.7/fileHashes/resourceHashesCache.bin  M java/resistor-color-trio/.gradle/buildOutputCleanup/buildOutputCleanup.lock  M java/resistor-color-trio/build/classes/java/main/ResistorColorTrio.class  M java/resistor-color-trio/build/reports/tests/test/classes/ResistorColorTrioTest.html  M java/resistor-color-trio/build/reports/tests/test/index.html  M java/resistor-color-trio/build/reports/tests/test/packages/default-package.html  M java/resistor-color-trio/build/test-results/test/TEST-ResistorColorTrioTest.xml  M java/resistor-color-trio/build/test-results/test/binary/results.bin  M java/resistor-color-trio/build/tmp/compileJava/compileTransaction/stash-dir/ResistorColorTrio.class.uniqueId0  M java/resistor-color-trio/build/tmp/compileJava/previous-compilation-data.bin  M java/resistor-color-trio/build/tmp/compileTestJava/compileTransaction/stash-dir/ResistorColorTrioTest.class.uniqueId0  M java/resistor-color-trio/build/tmp/compileTestJava/previous-compilation-data.bin  M java/resistor-color-trio/src/main/java/ResistorColorTrio.java

--- java/resistor-color-trio/src/main/java/ResistorColorTrio.java
+++ java/resistor-color-trio/src/main/java/ResistorColorTrio.java
@@ -17,34 +17,49 @@ class ResistorColorTrio {
         return colorCodes;
     }
 
-    private String getUnit(String color) {
-      int number = Integer.parseInt(color);
-      switch (number) {
-        case 9:
-          return " gigaohms";
-        case 8:
-          return "00 megaohms";
-        case 7:
-          return "0 megaohms";
-        case 6:
-          return " megaohms";
-        case 5:
-          return "00 kiloohms";
-        case 4:
-          return "0 kiloohms";
-        case 3:
-          return " kiloohms";
-        case 2:
-          return "00 ohms";
-        case 1:
-          return "0 ohms";
-      }
-      return " ohms";
+    private String getUnit(String zeroes) {
+        switch (zeroes.length()) {
+            case 9:
+                return " gigaohms";
+            case 8:
+                return "00 megaohms";
+            case 7:
+                return "0 megaohms";
+            case 6:
+                return " megaohms";
+            case 5:
+                return "00 kiloohms";
+            case 4:
+                return "0 kiloohms";
+            case 3:
+                return " kiloohms";
+            case 2:
+                return "00 ohms";
+            case 1:
+                return "0 ohms";
+            default:
+                return " ohms";
+        }
     }
 
     String label(String[] colors) {
-      HashMap<String, String> colorCodes = getColorCodes();
-      String code = colorCodes.get(colors[0]) + colorCodes.get(colors[1]) + getUnit(colorCodes.get(colors[2]));
-      return code;
+        HashMap<String, String> colorCodes = getColorCodes();
+        StringBuilder prefix = new StringBuilder();
+        StringBuilder suffix = new StringBuilder();
+
+        String color1 = colorCodes.get(colors[0]);
+        String color2 = colorCodes.get(colors[1]);
+        String color3 = colorCodes.get(colors[2]);
+
+        if (!color1.equals("0")) prefix.append(color1);
+        if (!color2.equals("0")) prefix.append(color2);
+        else suffix.append(color2);
+
+        for (int i = 0; i < Integer.parseInt(color3); i++) suffix.append("0");
+
+        String unit = getUnit(suffix.toString());
+        prefix.append(unit);
+
+        return prefix.toString();
     }
 }
