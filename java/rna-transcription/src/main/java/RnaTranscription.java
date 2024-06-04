6013c27bd422270370d4a008e550e7a93872dd3f
Irfan <irfansalim77@gmail.com>
2024-06-04 22:40:15 +0530

    Add  M java/rna-transcription/gradlew  M java/rna-transcription/src/main/java/RnaTranscription.java ?? java/rna-transcription/.gradle/ ?? java/rna-transcription/build/

--- java/rna-transcription/src/main/java/RnaTranscription.java
+++ java/rna-transcription/src/main/java/RnaTranscription.java
@@ -1,7 +1,23 @@
+import java.util.HashMap;
+
 class RnaTranscription {
 
     String transcribe(String dnaStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+      HashMap<Character, Character> dnaToRna = new HashMap<>();
+      dnaToRna.put('G', 'C');
+      dnaToRna.put('C', 'G');
+      dnaToRna.put('T', 'A');
+      dnaToRna.put('A', 'U');
+
+      StringBuilder rna = new StringBuilder();
+
+      for (Character c: dnaStrand.toCharArray()) {
+        if (dnaToRna.containsKey(c)) {
+          rna.append(dnaToRna.get(c));
+        } else rna.append(c);
+      }
+
+      return rna.toString();
     }
 
 }
