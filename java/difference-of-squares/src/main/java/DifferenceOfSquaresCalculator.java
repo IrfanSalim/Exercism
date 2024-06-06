ab18a1d905fca8ef9b5da04dd9d5a11b547d2f81
Irfan <irfansalim77@gmail.com>
2024-06-06 19:08:36 +0530

    Add  M java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java

--- java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
+++ java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
@@ -21,3 +21,42 @@ class DifferenceOfSquaresCalculator {
     }
 
 }
+
+
+import java.util.stream.IntStream;
+
+class DifferenceOfSquaresCalculator {
+
+    int computeSquareOfSumTo(int input) {
+        int sum = IntStream.rangeClosed(1, input).sum();
+        return sum * sum;
+    }
+    
+    int computeSumOfSquaresTo(int input) {
+        return IntStream.rangeClosed(1, input)
+            .map(num -> num * num)
+            .sum();
+    }
+    
+    int computeDifferenceOfSquares(int input) {
+        return computeSquareOfSumTo(input) - computeSumOfSquaresTo(input);
+    }
+}
+
+
+class DifferenceOfSquaresCalculator {
+
+    int computeSquareOfSumTo(int input) {
+        int sum = (input * (input + 1)) / 2;
+        return sum * sum;
+    }
+
+    int computeSumOfSquaresTo(int input) {
+        return (input * (input + 1) * ((input * 2) + 1)) / 6;
+    }
+
+    int computeDifferenceOfSquares(int input) {
+        return computeSquareOfSumTo(input) - computeSumOfSquaresTo(input);
+    }
+
+}
