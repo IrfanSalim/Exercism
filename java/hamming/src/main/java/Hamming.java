6f77824998edbc315b5f5b161bce22627b98255e
Irfan <irfansalim77@gmail.com>
2024-06-08 21:32:17 +0530

    Add  M java/hamming/src/main/java/Hamming.java

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -16,3 +16,67 @@ public class Hamming {
       return this.hammingDistance;
     }
 }
+
+
+import java.util.stream.IntStream;
+
+public class Hamming {
+
+    final private long difference;
+
+    public Hamming(String leftStrand, String rightStrand) {
+        if (leftStrand.length() != rightStrand.length()) {
+            throw new IllegalArgumentException("strands must be of equal length");
+        }
+        difference = IntStream
+            .range(0, leftStrand.length())
+            .filter(i -> leftStrand.charAt(i) != rightStrand.charAt(i))
+            .count();
+    }
+
+    public long getHammingDistance() {
+        return difference;
+    }
+}
+
+
+import java.util.stream.IntStream;
+
+public class Hamming {
+
+    final private int difference;
+
+    public Hamming(String leftStrand, String rightStrand) {
+        if (leftStrand.length() != rightStrand.length()) {
+            throw new IllegalArgumentException("strands must be of equal length");
+        }
+        difference = IntStream
+            .range(0, leftStrand.length())
+            .map(i -> leftStrand.charAt(i) != rightStrand.charAt(i) ? 1 : 0)
+            .sum();
+    }
+
+    public int getHammingDistance() {
+        return difference;
+    }
+}
+
+
+import java.util.stream.IntStream;
+
+public class Hamming {
+
+    final private int difference;
+
+    public Hamming(String leftStrand, String rightStrand) {
+        if (leftStrand.length() != rightStrand.length()) {
+            throw new IllegalArgumentException("strands must be of equal length");
+        }
+        difference = IntStream.range(0, leftStrand.length()).reduce(0,
+            (hamcount, index) -> hamcount + (leftStrand.charAt(index) != rightStrand.charAt(index) ? 1 : 0));
+    }
+
+    public int getHammingDistance() {
+        return difference;
+    }
+}
