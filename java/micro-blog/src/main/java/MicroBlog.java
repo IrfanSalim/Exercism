2471c3db9131f864c84be923ad1b5821dff76069
Irfan <irfansalim77@gmail.com>
2024-06-10 08:54:47 +0530

    Add  M java/micro-blog/src/main/java/MicroBlog.java

--- java/micro-blog/src/main/java/MicroBlog.java
+++ java/micro-blog/src/main/java/MicroBlog.java
@@ -4,3 +4,12 @@ class MicroBlog {
       return input.substring(0, input.offsetByCodePoints(0, usableLength));
     }
 }
+
+class MicroBlog {
+  public String truncate(String input) {
+    return input.codePoints().limit(5)
+        .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString();
+  }
+}
+
+
