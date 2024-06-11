01d77643a53257be6e46307ed9b2798be3e231d4
Irfan <irfansalim77@gmail.com>
2024-06-11 18:37:04 +0530

    Add  M java/pangram/gradlew  M java/pangram/src/main/java/PangramChecker.java  M java/pangram/src/test/java/PangramCheckerTest.java ?? java/pangram/.gradle/ ?? java/pangram/build/

--- java/pangram/src/main/java/PangramChecker.java
+++ java/pangram/src/main/java/PangramChecker.java
@@ -1,7 +1,14 @@
+import java.util.HashSet;
+
 public class PangramChecker {
 
     public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+      input = input.toLowerCase().replaceAll("[^a-z]", "");
+      HashSet<Character> letters = new HashSet<>();
+      for (int i = 0; i < input.length(); i++) {
+        letters.add(input.charAt(i));
+      }
+      return letters.size() == 26;
     }
 
 }
