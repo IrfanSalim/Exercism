3e461577b325f957129696a7bd464217dfd3a536
Irfan <irfansalim77@gmail.com>
2024-06-11 18:40:48 +0530

    Add  M java/pangram/src/main/java/PangramChecker.java

--- java/pangram/src/main/java/PangramChecker.java
+++ java/pangram/src/main/java/PangramChecker.java
@@ -12,3 +12,24 @@ public class PangramChecker {
     }
 
 }
+
+public class PangramChecker {
+    private final static int LETTERS_IN_ALPHABET = 26;
+
+    public boolean isPangram(String input) {
+        return input.toLowerCase().chars()
+            .filter(Character::isLetter)
+            .distinct()
+            .count() == LETTERS_IN_ALPHABET;
+    }
+}
+
+import java.util.Arrays;
+
+public class PangramChecker {
+
+    public boolean isPangram(String input) {
+        return Arrays.asList(input.toLowerCase().split(""))
+            .containsAll(Arrays.asList("abcdefghijklmnopqrstuvwxyz".split("")));
+    }
+}
