95ce65de731c766d0b4e71e7138bf4d4964ae7a0
Irfan <irfansalim77@gmail.com>
2024-07-11 19:32:49 +0530

    Add  M java/isbn-verifier/src/main/java/IsbnVerifier.java

--- java/isbn-verifier/src/main/java/IsbnVerifier.java
+++ java/isbn-verifier/src/main/java/IsbnVerifier.java
@@ -1,7 +1,17 @@
 class IsbnVerifier {
 
     boolean isValid(String stringToVerify) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+      int sum = 0;
+      int multiplier = 10;
+      for (int i = 0; i < stringToVerify.length(); i++) {
+        if (Character.isDigit(stringToVerify.charAt(i))) {
+          sum += (Integer.parseInt(String.valueOf(stringToVerify.charAt(i))) * multiplier);
+        } else if (stringToVerify.charAt(i) == 'X') {
+          sum += 10 * multiplier;
+        }
+        multiplier--;
     }
-
+    
+    return sum % 11 == 0;
+  }
 }
