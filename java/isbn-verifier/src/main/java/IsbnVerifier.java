a391061857b3e46fbca052b42bcdd23b4416d5b0
Irfan <irfansalim77@gmail.com>
2024-07-12 23:43:03 +0530

    Add  M java/isbn-verifier/src/main/java/IsbnVerifier.java

--- java/isbn-verifier/src/main/java/IsbnVerifier.java
+++ java/isbn-verifier/src/main/java/IsbnVerifier.java
@@ -31,3 +31,59 @@ class IsbnVerifier {
     }
 }
 
+
+
+import java.util.stream.IntStream;
+
+class IsbnVerifier {
+
+    public boolean isValid(String s) {
+        String scrubbed = s.replace("-", "");
+
+        return scrubbed.matches("^([0-9]{10}|[0-9]{9}X)$") &&
+            IntStream.range(0, scrubbed.length())
+            .map(pos -> {
+                var chr = scrubbed.charAt(pos);
+                return (chr != 'X' ? chr - '0' : 10) * (10 - pos);
+            })
+            .sum() % 11 == 0;
+    }
+}
+
+
+
+class IsbnVerifier {
+
+    boolean isValid(String stringToVerify) {
+        var acc = new IsbnAcc();
+        stringToVerify.chars().forEach(acc::calc);
+        return acc.isIsbn();
+    }
+
+}
+
+class IsbnAcc {
+    int sum = 0;
+    int pos = 0;
+    boolean allValidChars = true;
+
+    boolean isIsbn() {
+        return allValidChars && pos == 10 && sum % 11 == 0;
+    }
+
+    void calc(int codePoint) {
+        if (Character.isDigit(codePoint)) {
+            sum += (codePoint - '0') * (10 - pos);
+            pos++;
+            return;
+        }
+        if (codePoint == '-')
+            return;
+        if (codePoint == 'X' && pos == 9) {
+            sum += 10;
+            pos++;
+            return;
+        }
+        allValidChars = false;
+    }
+}
