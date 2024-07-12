b834a47178653f11fe624725d6d810e940559861
Irfan <irfansalim77@gmail.com>
2024-07-12 23:51:46 +0530

    Add  M java/isogram/.gradle/8.7/executionHistory/executionHistory.bin  M java/isogram/.gradle/8.7/executionHistory/executionHistory.lock  M java/isogram/.gradle/8.7/fileHashes/fileHashes.bin  M java/isogram/.gradle/8.7/fileHashes/fileHashes.lock  M java/isogram/.gradle/8.7/fileHashes/resourceHashesCache.bin  M java/isogram/.gradle/buildOutputCleanup/buildOutputCleanup.lock  M java/isogram/build/classes/java/main/IsogramChecker.class  M java/isogram/build/reports/tests/test/classes/IsogramCheckerTest.html  M java/isogram/build/reports/tests/test/index.html  M java/isogram/build/reports/tests/test/packages/default-package.html  M java/isogram/build/test-results/test/TEST-IsogramCheckerTest.xml  M java/isogram/build/test-results/test/binary/results.bin  M java/isogram/build/tmp/compileJava/previous-compilation-data.bin  M java/isogram/src/main/java/IsogramChecker.java ?? java/isogram/build/tmp/compileJava/compileTransaction/

--- java/isogram/src/main/java/IsogramChecker.java
+++ java/isogram/src/main/java/IsogramChecker.java
@@ -1,7 +1,36 @@
 class IsogramChecker {
 
     boolean isIsogram(String phrase) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+      phrase = phrase.replaceAll("[^a-zA-Z]", "").toLowerCase();
+      return phrase.toLowerCase().chars().distinct().count() == phrase.length();
     }
 
+}
+
+
+import java.util.HashSet;
+
+final class IsogramChecker {
+
+    boolean isIsogram(final String phrase) {
+        return phrase.chars()
+            .filter(Character::isLetter)
+            .map(Character::toLowerCase)
+            .allMatch(new HashSet < > ()::add);
+    }
+}
+
+
+import java.util.stream.Collectors;
+
+public class IsogramChecker {
+
+    public boolean isIsogram(String input) {
+        final var scrubbed = input.chars()
+            .filter(Character::isLetter)
+            .mapToObj(Character::toLowerCase)
+            .collect(Collectors.toList());
+
+        return scrubbed.size() == scrubbed.stream().distinct().count();
+    }
 }
