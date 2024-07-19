cef0f2da4540f7f0e0d00b3ec2644c79283868a0
Irfan <irfansalim77@gmail.com>
2024-07-19 18:39:13 +0530

    Add  M java/sum-of-multiples/src/main/java/SumOfMultiples.java ?? java/sum-of-multiples/.gradle/ ?? java/sum-of-multiples/build/

--- java/sum-of-multiples/src/main/java/SumOfMultiples.java
+++ java/sum-of-multiples/src/main/java/SumOfMultiples.java
@@ -1,11 +1,24 @@
-class SumOfMultiples {
+import java.util.HashSet;
+import java.util.Set;
 
-    SumOfMultiples(int number, int[] set) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+public class SumOfMultiples {
+    private final int limit;
+    private final int[] factors;
+
+    public SumOfMultiples(int limit, int[] factors) {
+        this.limit = limit;
+        this.factors = factors;
     }
 
-    int getSum() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    public int getSum() {
+        Set<Integer> multiples = new HashSet<>();
+        for (int factor : factors) {
+            if (factor != 0) {
+                for (int i = factor; i < limit; i += factor) {
+                    multiples.add(i);
+                }
+            }
+        }
+        return multiples.stream().mapToInt(Integer::intValue).sum();
     }
-
 }
