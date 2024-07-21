73ce431b2388d184e2d39291419135d7d8e61dee
Irfan <irfansalim77@gmail.com>
2024-07-21 23:09:10 +0530

    Add  M java/matching-brackets/gradlew  M java/matching-brackets/src/main/java/BracketChecker.java  M java/matching-brackets/src/test/java/BracketCheckerTest.java ?? java/matching-brackets/.gradle/ ?? java/matching-brackets/build/

--- java/matching-brackets/src/main/java/BracketChecker.java
+++ java/matching-brackets/src/main/java/BracketChecker.java
@@ -1,11 +1,37 @@
+import java.util.*;
+
 class BracketChecker {
 
+    String expression;
+
     BracketChecker(String expression) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.expression = expression;
     }
 
     boolean areBracketsMatchedAndNestedCorrectly() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        char[] chars = expression.toCharArray();
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == '(' || chars[i] == '[' || chars[i] == '{') {
+                stack.push(chars[i]);
+            } else if (chars[i] == ')' || chars[i] == ']' || chars[i] == '}') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                if (isMatchingPair(stack.peek(), chars[i])) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    boolean isMatchingPair(Character open, char close) {
+        return (open == '(' && close == ')') || (open == '[' && close == ']') || (open == '{' && close == '}');
     }
 
 }
