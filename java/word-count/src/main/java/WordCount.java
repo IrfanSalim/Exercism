1a4a057a95be8be9e37975692eb65dece65ed5f4
Irfan <irfansalim77@gmail.com>
2024-07-24 20:37:55 +0530

    Add  M java/word-count/src/main/java/WordCount.java

--- java/word-count/src/main/java/WordCount.java
+++ java/word-count/src/main/java/WordCount.java
@@ -19,3 +19,23 @@ class WordCount {
     }
 }
 
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+class WordCount {
+    public Map<String,Integer> phrase(String word) {
+        return Arrays.stream(word
+                .toLowerCase()
+                .replaceAll("[^a-z0-9' ]", " ")
+                .trim()
+                .split("\\s+"))
+            .map(it -> it.replaceAll("^'|'$", ""))
+            .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+            .entrySet().stream()
+            .collect(Collectors.toMap(Map.Entry::getKey, it -> it.getValue().intValue()));
+    }
+}
+
