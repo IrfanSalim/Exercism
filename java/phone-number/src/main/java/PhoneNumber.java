e3682562ca49087cd241ccbdb4019209b68f2040
Irfan <irfansalim77@gmail.com>
2024-07-30 17:43:51 +0530

    Add  M java/phone-number/src/main/java/PhoneNumber.java ?? java/phone-number/.gradle/ ?? java/phone-number/build/

--- java/phone-number/src/main/java/PhoneNumber.java
+++ java/phone-number/src/main/java/PhoneNumber.java
@@ -1,11 +1,31 @@
-class PhoneNumber {
+import java.util.Map;
 
-    PhoneNumber(String numberString) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+public class PhoneNumber {
+    private static Map<Character, String> codes = Map.of('0', "zero", '1', "one");
+    private String number;
+
+    public PhoneNumber(String num) {
+        this.number = num.replaceAll("[()\\-.+\\s]", "");
+        if (number.matches("^\\d*\\p{IsLatin}+\\d*$"))
+            throw new IllegalArgumentException("letters not permitted");
+        if (number.matches("^\\d*\\p{Punct}+\\d*$"))
+            throw new IllegalArgumentException("punctuations not permitted");
+        if (number.length() == 11) {
+            if (!number.startsWith("1"))
+                throw new IllegalArgumentException("11 digits must start with 1");
+            number = number.substring(1);
+        }
+        if (number.length() > 11)
+            throw new IllegalArgumentException("must not be greater than 11 digits");
+        if (number.length() < 10)
+            throw new IllegalArgumentException("must not be fewer than 10 digits");
+        if (!number.matches("\\d{3}[2-9]\\d+"))
+            throw new IllegalArgumentException("exchange code cannot start with %s".formatted(codes.get(number.charAt(3))));
+        if (!number.matches("[2-9]\\d+"))
+            throw new IllegalArgumentException("area code cannot start with %s".formatted(codes.get(number.charAt(0))));
     }
 
-    String getNumber() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    public String getNumber() {
+        return number;
     }
-
 }
