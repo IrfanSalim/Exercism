898f513a576cf025c63d87ee6be5d3859e9eea5b
Irfan <irfansalim77@gmail.com>
2024-07-30 17:53:27 +0530

    Add  M java/phone-number/src/main/java/PhoneNumber.java

--- java/phone-number/src/main/java/PhoneNumber.java
+++ java/phone-number/src/main/java/PhoneNumber.java
@@ -29,3 +29,34 @@ public class PhoneNumber {
         return number;
     }
 }
+
+
+class PhoneNumber {
+	private String number;
+
+	public PhoneNumber(String rawNumber) {
+		number = rawNumber.replaceAll("[ ()+-.]", "");
+
+		if (number.matches(".*[a-zA-Z]+.*")) throw new IllegalArgumentException("letters not permitted");
+		if (number.matches(".*\\D+.*")) throw new IllegalArgumentException("punctuations not permitted");
+
+		if (number.length() > 11) throw new IllegalArgumentException("more than 11 digits");
+		if (number.length() == 11) {
+			if (!number.startsWith("1")) throw new IllegalArgumentException("11 digits must start with 1");
+			number = number.substring(1);
+		}
+		if (number.length() < 10) throw new IllegalArgumentException("incorrect number of digits");
+
+		if (number.startsWith("0")) throw new IllegalArgumentException("area code cannot start with zero");
+		if (number.startsWith("1")) throw new IllegalArgumentException("area code cannot start with one");
+		if (number.charAt(3) == '0') throw new IllegalArgumentException("exchange code cannot start with zero");
+		if (number.charAt(3) == '1') throw new IllegalArgumentException("exchange code cannot start with one");
+	}
+
+	public String getNumber() {
+		return number;
+	}
+}
+
+
+
