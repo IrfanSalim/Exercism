84ebd25dc1a873e95248d4399b6391540922cc83
Irfan <irfansalim77@gmail.com>
2024-08-02 00:16:29 +0530

    Add  M java/bob/.gradle/8.7/executionHistory/executionHistory.bin  M java/bob/.gradle/8.7/executionHistory/executionHistory.lock  M java/bob/.gradle/8.7/fileHashes/fileHashes.bin  M java/bob/.gradle/8.7/fileHashes/fileHashes.lock  M java/bob/.gradle/8.7/fileHashes/resourceHashesCache.bin  M java/bob/.gradle/buildOutputCleanup/buildOutputCleanup.lock  M java/bob/build/classes/java/main/Bob.class  M java/bob/build/reports/tests/test/classes/BobTest.html  M java/bob/build/reports/tests/test/index.html  M java/bob/build/reports/tests/test/packages/default-package.html  M java/bob/build/test-results/test/TEST-BobTest.xml  M java/bob/build/test-results/test/binary/results.bin  M java/bob/build/tmp/compileJava/compileTransaction/stash-dir/Bob.class.uniqueId0  M java/bob/build/tmp/compileJava/previous-compilation-data.bin  M java/bob/build/tmp/compileTestJava/previous-compilation-data.bin  M java/bob/src/main/java/Bob.java ?? java/bob/build/tmp/compileTestJava/compileTransaction/

--- java/bob/src/main/java/Bob.java
+++ java/bob/src/main/java/Bob.java
@@ -1,29 +1,31 @@
 class Bob {
 
     String hey(String input) {
-      if (input == null || input.trim().length() == 0) {
-        return "Fine. Be that way!";
-      } else if (isYell(input)) {
-        return "Whoa, chill out!";
-      } else if (isYellQuestion(input)) {
-        return "Calm down, I know what I'm doing!";
-      } else if (isQuestion(input)) {
-        return "Sure.";
-      } else {
-        return "Whatever.";
-      }
+        if (input == null || input.trim().length() == 0) {
+            return "Fine. Be that way!";
+        } else if (isYell(input)) {
+            return "Whoa, chill out!";
+        } else if (isYellQuestion(input)) {
+            return "Calm down, I know what I'm doing!";
+        } else if (isQuestion(input)) {
+            return "Sure.";
+        } else {
+            return "Whatever.";
+        }
     }
 
     boolean isQuestion(String input) {
-      return input.trim().endsWith("?");
+        return input.trim().endsWith("?");
     }
 
     boolean isYell(String input) {
-      return input.toUpperCase().equals(input) && !isQuestion(input);
+        boolean hasLetters = input.chars().anyMatch(Character::isLetter);
+        return hasLetters && input.equals(input.toUpperCase()) && !isQuestion(input);
     }
 
     boolean isYellQuestion(String input) {
-      return input.toUpperCase().equals(input) && isQuestion(input);
+        boolean hasLetters = input.chars().anyMatch(Character::isLetter);
+        return hasLetters && input.equals(input.toUpperCase()) && isQuestion(input);
     }
-
 }
+
