fb6d3b9de7a0ac8d0e3238b9899d2756e73d6872
Irfan <irfansalim77@gmail.com>
2024-08-01 20:55:59 +0530

    Add  M java/bob/gradlew  M java/bob/src/main/java/Bob.java ?? java/bob/.gradle/ ?? java/bob/build/

--- java/bob/src/main/java/Bob.java
+++ java/bob/src/main/java/Bob.java
@@ -1,7 +1,29 @@
 class Bob {
 
     String hey(String input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+      if (input == null || input.trim().length() == 0) {
+        return "Fine. Be that way!";
+      } else if (isYell(input)) {
+        return "Whoa, chill out!";
+      } else if (isYellQuestion(input)) {
+        return "Calm down, I know what I'm doing!";
+      } else if (isQuestion(input)) {
+        return "Sure.";
+      } else {
+        return "Whatever.";
+      }
+    }
+
+    boolean isQuestion(String input) {
+      return input.trim().endsWith("?");
+    }
+
+    boolean isYell(String input) {
+      return input.toUpperCase().equals(input) && !isQuestion(input);
+    }
+
+    boolean isYellQuestion(String input) {
+      return input.toUpperCase().equals(input) && isQuestion(input);
     }
 
 }
