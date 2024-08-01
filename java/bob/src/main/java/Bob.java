6e197d88fca6c9b79bbc6d0b081f5a4fae5d4eec
Irfan <irfansalim77@gmail.com>
2024-08-02 00:25:21 +0530

    Add  M java/bob/src/main/java/Bob.java

--- java/bob/src/main/java/Bob.java
+++ java/bob/src/main/java/Bob.java
@@ -29,3 +29,55 @@ class Bob {
     }
 }
 
+
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+
+class Bob {
+
+    final private static Pattern isAlpha = Pattern.compile("[a-zA-Z]");
+    final private static Predicate < String > isShout = msg -> isAlpha.matcher(msg).find() && msg == msg.toUpperCase();
+
+    public String hey(String message) {
+        var speech = message.trim();
+        if (speech.isEmpty()) {
+            return "Fine. Be that way!";
+        }
+        var questioning = speech.endsWith("?");
+        var shouting = isShout.test(speech);
+        if (questioning) {
+            if (shouting) {
+                return "Calm down, I know what I'm doing!";
+            }
+            return "Sure.";
+        }
+        if (shouting) {
+            return "Whoa, chill out!";
+        }
+        return "Whatever.";
+    }
+}
+
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+
+class Bob {
+    final private static String[] answers = {
+        "Whatever.",
+        "Sure.",
+        "Whoa, chill out!",
+        "Calm down, I know what I'm doing!"
+    };
+    final private static Pattern isAlpha = Pattern.compile("[a-zA-Z]");
+    final private static Predicate < String > isShout = msg -> isAlpha.matcher(msg).find() && msg == msg.toUpperCase();
+
+    public String hey(String message) {
+        var speech = message.trim();
+        if (speech.isEmpty()) {
+            return "Fine. Be that way!";
+        }
+        var questioning = speech.endsWith("?") ? 1 : 0;
+        var shouting = isShout.test(speech) ? 2 : 0;
+        return answers[questioning + shouting];
+    }
+}
