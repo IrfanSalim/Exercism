d391e36e326f1a4cbbceeb64c6c63204c6b6eb28
Irfan <irfansalim77@gmail.com>
2024-08-03 10:37:16 +0530

    Add  M java/square-root/src/main/java/SquareRoot.java

--- java/square-root/src/main/java/SquareRoot.java
+++ java/square-root/src/main/java/SquareRoot.java
@@ -7,3 +7,16 @@ public class SquareRoot {
       return i - 1;
     }
 }
+
+import java.util.stream.IntStream;
+
+public class SquareRoot
+{
+    public int squareRoot(int radicand)
+    {
+        return IntStream.range(1, radicand + 1)
+                .filter(i -> i * i == radicand)
+                .findFirst()
+                .orElse(0);
+    }
+}
