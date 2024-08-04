9e5ed741ae796dc88d8c0c330a47a1ed2d47fe87
Irfan <irfansalim77@gmail.com>
2024-08-04 15:17:27 +0530

    Add  M java/spiral-matrix/src/main/java/SpiralMatrixBuilder.java ?? java/spiral-matrix/.gradle/ ?? java/spiral-matrix/build/

--- java/spiral-matrix/src/main/java/SpiralMatrixBuilder.java
+++ java/spiral-matrix/src/main/java/SpiralMatrixBuilder.java
@@ -1,5 +1,23 @@
 class SpiralMatrixBuilder {
     int[][] buildMatrixOfSize(int size) {
-        throw new UnsupportedOperationException("Please implement the SpiralMatrixBuilder.buildMatrixOfSize() method.");
+      int[][] matrix = new int[size][size];
+      int counter = 1;
+
+      for (int i = 0; i < size; i++) {
+        for (int j = i; j < size - i; j++) {
+          matrix[i][j] = counter++;
+        }
+        for (int j = i + 1; j < size - i; j++) {
+          matrix[j][size - i - 1] = counter++;
+        }
+        for (int j = size - i - 2; j >= i; j--) {
+          matrix[size - i - 1][j] = counter++;
+        }
+        for (int j = size - i - 2; j > i; j--) {
+          matrix[j][i] = counter++;
+        }
+      }
+      
+      return matrix;
     }
 }
