9f12a65b2b0cae2a501181dd13a50104cd8b0cc4
Irfan <irfansalim77@gmail.com>
2024-08-07 14:51:17 +0530

    Add  M java/nucleotide-count/gradlew  M java/nucleotide-count/src/main/java/NucleotideCounter.java ?? java/nucleotide-count/.gradle/ ?? java/nucleotide-count/build/

--- java/nucleotide-count/src/main/java/NucleotideCounter.java
+++ java/nucleotide-count/src/main/java/NucleotideCounter.java
@@ -1,13 +1,29 @@
+import java.util.HashMap;
 import java.util.Map;
 
 class NucleotideCounter {
 
+    String sequence;
     NucleotideCounter(String sequence) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.sequence = sequence.toUpperCase();
+        if(!this.sequence.matches("[ACGT]*"))
+        {
+            throw new IllegalArgumentException();
+        }    
     }
 
     Map<Character, Integer> nucleotideCounts() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        Map<Character, Integer> nucleotideCounts = new HashMap<>();
+        nucleotideCounts.put('A', 0);
+        nucleotideCounts.put('T', 0);
+        nucleotideCounts.put('C', 0);
+        nucleotideCounts.put('G', 0);
+
+        for (char nucleotide : sequence.toCharArray()) {
+            nucleotideCounts.put(nucleotide, nucleotideCounts.get(nucleotide) + 1);
+        }
+        return nucleotideCounts;
     }
 
 }
+
