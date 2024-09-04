a3cde4559abd17cb5dfd5279bc84b00977db03fa
Irfan <irfansalim77@gmail.com>
2024-09-04 20:50:29 +0530

    Add  M java/sieve/gradlew  M java/sieve/src/main/java/Sieve.java ?? java/sieve/.gradle/ ?? java/sieve/build/

--- java/sieve/src/main/java/Sieve.java
+++ java/sieve/src/main/java/Sieve.java
@@ -1,11 +1,26 @@
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 class Sieve {
+    int maxPrime;
+
     Sieve(int maxPrime) {
-        throw new UnsupportedOperationException("Delete this statement and provide your own implementation.");
+      this.maxPrime = maxPrime;
     }
 
     List<Integer> getPrimes() {
-        throw new UnsupportedOperationException("Delete this statement and provide your own implementation.");
+      List<Integer> primes = new ArrayList<>();
+      boolean[] sieve = new boolean[maxPrime + 1];
+      Arrays.fill(sieve, true);
+      for (int i = 2; i <= maxPrime; i++) {
+        if (sieve[i]) {
+          primes.add(i);
+          for (int j = i + i; j <= maxPrime; j += i) {
+            sieve[j] = false;
+          }
+        }
+      }
+      return primes;
     }
 }
