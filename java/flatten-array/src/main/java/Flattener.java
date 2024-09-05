b22f47ffb62bdd92c54bf022110892e8b362809e
Irfan <irfansalim77@gmail.com>
2024-09-05 20:17:24 +0530

    Add  M java/flatten-array/gradlew  M java/flatten-array/src/main/java/Flattener.java ?? java/flatten-array/.gradle/ ?? java/flatten-array/build/

--- java/flatten-array/src/main/java/Flattener.java
+++ java/flatten-array/src/main/java/Flattener.java
@@ -1,9 +1,18 @@
 import java.util.List;
+import java.util.ArrayList;
 
 class Flattener {
 
     <T> List<T> flatten(List<T> list) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        List<T> result = new ArrayList<>();
+        for (T t : list) {
+            if (t instanceof List) {
+                result.addAll(flatten((List<T>) t));
+            } else if (t != null) {
+                result.add(t);
+            }
+        }
+        return result;
     }
 
 }
