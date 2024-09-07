ff0063b29e9775a6787fff5902bfa291cd40df96
Irfan <irfansalim77@gmail.com>
2024-09-08 00:01:33 +0530

    Add  M java/anagram/gradlew  M java/anagram/src/main/java/Anagram.java ?? java/anagram/.gradle/ ?? java/anagram/build/

--- java/anagram/src/main/java/Anagram.java
+++ java/anagram/src/main/java/Anagram.java
@@ -1,12 +1,39 @@
 import java.util.List;
+import java.util.ArrayList;
 
 class Anagram {
+    String anagram;
+    int[] charCount = new int[26];
     public Anagram(String word) {
-        throw new UnsupportedOperationException("Please implement the Anagram(String word) constructor");
+      this.anagram = word;
+      word = word.toLowerCase();
+      for (int i = 0; i < word.length(); i++) {
+        charCount[word.charAt(i) - 'a']++;
+      }
     }
 
     public List<String> match(List<String> candidates) {
-        throw new UnsupportedOperationException("Please implement the match() method");
+      List<String> result = new ArrayList<>();
+      for (String candidate : candidates) {
+        candidate = candidate.toLowerCase();
+        if (anagram.length() == candidate.length()) {
+          int[] candidateCharCount = new int[26];
+          for (int i = 0; i < candidate.length(); i++) {
+            candidateCharCount[candidate.charAt(i) - 'a']++;
+          }
+          boolean match = true;
+          for (int i = 0; i < 26; i++) {
+            if (charCount[i] != candidateCharCount[i]) {
+              match = false;
+              break;
+            }
+          }
+          if (match) {
+            result.add(candidate);
+          }
+        }
+      }
+      return result;
     }
 
 }
