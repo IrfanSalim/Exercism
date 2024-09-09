3ed9fd037668615a1e8a77e6b118d4aa355b1033
Irfan <irfansalim77@gmail.com>
2024-09-09 18:12:34 +0530

    Add  M java/diamond/bin/main/DiamondPrinter.class  M java/diamond/src/main/java/DiamondPrinter.java

--- java/diamond/src/main/java/DiamondPrinter.java
+++ java/diamond/src/main/java/DiamondPrinter.java
@@ -58,3 +58,59 @@ class DiamondPrinter {
     }
 
 }
+
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DiamondPrinter {
+
+    public List<String> printToList(char letter) {
+        int number = letter - 'A' + 1;
+        int size = 2 * (number - 1) + 1;
+
+        String[] lines = new String[size];
+        for (int i=0; i<size / 2 + 1; i++) {
+            char c = (char) ('A' + i);
+
+            int leftPosition = size / 2 + 1 - i - 1;
+            int rightPosition = size - leftPosition - 1;
+            char[] buffer = new char[size];
+            Arrays.fill(buffer, ' ');
+            buffer[leftPosition] = c;
+            buffer[rightPosition] = c;
+            String line = new String(buffer);
+
+            lines[i] = line;
+            lines[size - 1 - i] = line;
+        }
+
+        return Arrays.asList(lines);
+    }
+
+}
+
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+final class DiamondPrinter {
+    public static final char FROM = 'A';
+
+    public static String line(int x, int y) {
+        final int i = x - FROM;
+        return IntStream.rangeClosed(FROM - i, FROM + i)
+                .map(j -> FROM + Math.abs(FROM - j) == y? y: ' ')
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                    .toString();
+    }
+
+    public List<String> printToList(char x) {
+        return IntStream.concat(
+                IntStream.rangeClosed(FROM, x),
+                IntStream.iterate(x - 1, y -> y - 1).limit(x - FROM))
+                .mapToObj(y -> line(x, y))
+                .collect(Collectors.toList());
+    }
+}
