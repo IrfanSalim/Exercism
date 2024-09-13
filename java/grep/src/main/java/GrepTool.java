6ea773997b9731df40bfbdab6ba8e0bebaa92852
Irfan <irfansalim77@gmail.com>
2024-09-13 18:11:34 +0530

    Add  M java/grep/src/main/java/GrepTool.java

--- java/grep/src/main/java/GrepTool.java
+++ java/grep/src/main/java/GrepTool.java
@@ -45,3 +45,134 @@ class GrepTool {
     }
 
 }
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toCollection;
+
+public class GrepTool {
+
+    private String pattern;
+    private List<String> flags;
+    private SortedSet<File> files;
+    private Predicate<String> filter;
+
+    public String grep(String pattern, List<String> flags, List<String> fileNames) {
+        initFields(pattern, flags, fileNames);
+        handleMultipleFiles();
+        handleLineNumberFlag();
+
+        return needOnlyFileNames() ? getMatchingFiles() : getMatchingLines();
+    }
+
+    private void initFields(String pattern, List<String> flags, List<String> fileNames) {
+        this.pattern = pattern;
+        this.flags = flags;
+        files = fileNames.stream().map(File::new).collect(toCollection(TreeSet::new));
+        initFilter();
+    }
+
+    private void initFilter() {
+        filter = line -> line.contains(pattern);
+        if(flags.contains("-i")) {
+            filter = line -> line.toLowerCase().contains(pattern.toLowerCase());
+        }
+        if(flags.contains("-x")) {
+            filter = line -> line.equals(pattern);
+        }
+        if(flags.contains("-x") && flags.contains("-i")) {
+            filter = line -> line.equalsIgnoreCase(pattern);
+        }
+        if(flags.contains("-v")) {
+            filter = filter.negate();
+        }
+    }
+
+    private void handleMultipleFiles() {
+        if(files.size() > 1) {
+            files.forEach(File::appendFileName);
+        }
+    }
+
+    private void handleLineNumberFlag() {
+        if(flags.contains("-n")) {
+            files.forEach(File::appendNumbers);
+        }
+    }
+
+    private boolean needOnlyFileNames() {
+        return flags.contains("-l");
+    }
+
+    private String getMatchingFiles() {
+        return files.stream()
+                .filter(File::isMatching)
+                .map(file -> file.name)
+                .collect(joining("\n"));
+    }
+
+    private String getMatchingLines() {
+        return files.stream()
+                .map(File::getMatchingLines)
+                .flatMap(List::stream)
+                .collect(joining("\n"));
+    }
+
+     class File implements Comparable<File> {
+
+        private final String name;
+        private final List<String> lines;
+
+        private Function<String, String> appendNumbers = identity();
+        private Function<String, String> appendName = identity();
+
+        File(String name) {
+            this.name = name;
+
+            Path path = Paths.get(name);
+            try(Stream<String> lines = Files.lines(path)) {
+                this.lines = lines.toList();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        List<String> getMatchingLines() {
+            return lines.stream()
+                    .filter(filter)
+                    .map(appendNumbers)
+                    .map(appendName)
+                    .toList();
+        }
+
+        boolean isMatching() {
+            return lines.stream().anyMatch(filter);
+        }
+
+        void appendNumbers() {
+            appendNumbers = line -> 1 + lines.indexOf(line) + ":" + line;
+        }
+
+        void appendFileName() {
+            appendName = line -> name + ":" + line;
+        }
+
+
+        @Override
+        public int compareTo(File other) {
+            return name.compareTo(other.name);
+        }
+    }
+
+}
