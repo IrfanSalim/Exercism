5105a99e2afd3fca35e970bdf3a3c654d37b5024
Irfan <irfansalim77@gmail.com>
2024-09-16 17:06:55 +0530

    Add  M java/linked-list/src/main/java/DoublyLinkedList.java ?? java/linked-list/.gradle/ ?? java/linked-list/build/

--- java/linked-list/src/main/java/DoublyLinkedList.java
+++ java/linked-list/src/main/java/DoublyLinkedList.java
@@ -1,12 +1,23 @@
 class DoublyLinkedList<T> {
     private Element<T> head;
+    private Element<T> tail;
+
+    public DoublyLinkedList() {
+        this.head = new Element<>(null, null, tail);
+        this.tail = new Element<>(null, head, null);
+    }
 
     void push(T value) {
-        throw new UnsupportedOperationException("Please implement the DoublyLinkedList.push() method.");
+        Element<T> node = new Element<>(value, this.tail.prev, this.tail);
+        this.tail.prev.next = node;
+        this.tail.prev = node;
     }
 
     T pop() {
-        throw new UnsupportedOperationException("Please implement the DoublyLinkedList.pop() method.");
+        Element<T> node = this.tail.prev;
+        this.tail.prev = node.prev;
+        this.tail.prev.next = this.tail;
+        return (T) node.value;
     }
 
     void unshift(T value) {
@@ -23,7 +34,9 @@ class DoublyLinkedList<T> {
         private Element<T> next;
 
         Element(T value, Element<T> prev, Element<T> next) {
-            throw new UnsupportedOperationException("Please implement the Element constructor.");
+            this.value = value;
+            this.prev = prev;
+            this.next = next;
         }
     }
 }
