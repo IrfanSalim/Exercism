b14edc5c515833910fae88d4658c8020b06cc6e8
Irfan <irfansalim77@gmail.com>
2024-09-21 19:18:36 +0530

    Add  M java/prime-factors/src/main/java/PrimeFactorsCalculator.java ?? java/prime-factors/.gradle/ ?? java/prime-factors/build/

--- java/prime-factors/src/main/java/PrimeFactorsCalculator.java
+++ java/prime-factors/src/main/java/PrimeFactorsCalculator.java
@@ -1,9 +1,36 @@
-import java.util.List;
+import java.util.*;
 
 class PrimeFactorsCalculator {
 
     List<Long> calculatePrimeFactorsOf(long number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        List<Long> result = new ArrayList<Long>();
+        if (number <= 1) return result; // Early return for numbers <= 1
+        List<Long> primes = getPrimeNumbers((long) Math.sqrt(number) + 1);
+        for (Long prime : primes) {
+            while (number % prime == 0) {
+                result.add(prime);
+                number /= prime;
+            }
+        }
+        if (number > 1) result.add(number); // If number is prime itself
+        return result;
+    }
+
+    private List<Long> getPrimeNumbers(Long max) {
+        List<Long> result = new ArrayList<Long>();
+        Boolean[] primes = new Boolean[max.intValue() + 1];
+        Arrays.fill(primes, true);
+
+        for (int i = 2; i <= max; i++) {
+            if (primes[i]) {
+                for (int j = i * 2; j <= max; j += i) { // Start from i*2 and increment by i
+                    primes[j] = false;
+                }
+                result.add((long) i);
+            }
+        }
+
+        return result;
     }
 
 }
