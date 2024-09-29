adbc0a58edb78505ed0972720b251b3896010ac2
Irfan Salim <irfansalim77@gmail.com>
2024-09-29 23:07:52 +0530

    Add  M java/transpose/src/main/java/Transpose.java

--- java/transpose/src/main/java/Transpose.java
+++ java/transpose/src/main/java/Transpose.java
@@ -22,3 +22,36 @@ public class Transpose {
         return String.join("\n", columns);
     }
 }
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+class Transpose {
+
+    String transpose(final String input) {
+
+        final String[][] matrix = Stream.of(input.split("\\n"))
+                .map(row -> row.split("")).toArray(String[][]::new);
+
+        final int maxLength = Stream.of(matrix).mapToInt(i -> i.length).max().orElse(0);
+
+        final String[][] transposedMatrix = IntStream.range(0, maxLength)
+                .mapToObj(col -> Arrays.stream(matrix).map(row -> col < row.length ? row[col] : "")
+                        .toArray(String[]::new))
+                .toArray(String[][]::new);
+
+        return Stream.of(transposedMatrix).map(row -> String.join("", this.pad(row))).collect(Collectors.joining("\n"));
+
+    }
+
+    private String[] pad(final String[] row) {
+        final String[] paddedRow = new String[row.length];
+        paddedRow[row.length - 1] = row[row.length - 1];
+        for (int i = paddedRow.length - 2; i >= 0; i--) {
+            paddedRow[i] = row[i].equals("") && !paddedRow[i + 1].equals("") ? " " : row[i];
+        }
+        return paddedRow;
+    }
+}
