e9e3bf80bb4674da0d156ba910a8ba637134c3a0
Irfan Salim <irfansalim77@gmail.com>
2024-10-03 00:15:11 +0530

    Add  M java/grade-school/src/main/java/School.java

--- java/grade-school/src/main/java/School.java
+++ java/grade-school/src/main/java/School.java
@@ -30,3 +30,23 @@ class School {
     }
 
 }
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class School {
+
+    private final SortedMap<Integer, SortedSet<String>> roster = new TreeMap<>();
+
+    public void add(String name, int grade) {
+        roster.computeIfAbsent(grade, i -> new TreeSet<>()).add(name);
+    }
+
+    public List<String> roster() {
+        return roster.values().stream().flatMap(Collection::stream).collect(Collectors.toList());
+    }
+
+    public List<String> grade(int grade) {
+        return new ArrayList<>(roster.getOrDefault(grade, Collections.emptySortedSet()));
+    }
+}
