628fadb9c416258810a99fbaf99be219ec92f1d9
Irfan Salim <irfansalim77@gmail.com>
2024-10-04 19:42:59 +0530

    Add  M java/clock/gradlew  M java/clock/src/main/java/Clock.java ?? java/clock/.gradle/ ?? java/clock/build/

--- java/clock/src/main/java/Clock.java
+++ java/clock/src/main/java/Clock.java
@@ -1,21 +1,24 @@
-class Clock {
+public class Clock {
+    private static final int MINS_IN_HOUR = 60;
+    private static final int MINS_IN_DAY = MINS_IN_HOUR * 24;
 
-    Clock(int hours, int minutes) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    private int mins = 0;
+
+    public Clock(int h, int m) {
+        this.add(h * MINS_IN_HOUR + m);
     }
 
-    void add(int minutes) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    public void add(int m) {
+        mins = (MINS_IN_DAY + (mins + m) % MINS_IN_DAY) % MINS_IN_DAY;
     }
 
     @Override
     public String toString() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return String.format("%1$02d:%2$02d", (int) (mins / MINS_IN_HOUR), mins % MINS_IN_HOUR);
     }
 
     @Override
     public boolean equals(Object obj) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return obj != null && (obj instanceof Clock) && mins == ((Clock) obj).mins;
     }
-
 }
