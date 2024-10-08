ba873f6e10ba28a1c08d992e061b91ca608bb714
Irfan Salim <irfansalim77@gmail.com>
2024-10-08 23:38:08 +0530

    Add  M java/knapsack/gradlew  M java/knapsack/src/main/java/Knapsack.java ?? java/knapsack/.gradle/ ?? java/knapsack/build/

--- java/knapsack/src/main/java/Knapsack.java
+++ java/knapsack/src/main/java/Knapsack.java
@@ -3,7 +3,24 @@ import java.util.List;
 class Knapsack {
 
     int maximumValue(int maximumWeight, List<Item> items) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+        int n = items.size();
+        // Create a 2D array to store the maximum value at each n and w
+        int[][] dp = new int[n + 1][maximumWeight + 1];
 
+        // Build the dp array
+        for (int i = 1; i <= n; i++) {
+            for (int w = 0; w <= maximumWeight; w++) {
+                // If the item can be included in the knapsack
+                if (items.get(i - 1).weight <= w) {
+                    dp[i][w] = Math.max(dp[i - 1][w], dp[i - 1][w - items.get(i - 1).weight] + items.get(i - 1).value);
+                } else {
+                    // If the item cannot be included, carry forward the previous maximum value
+                    dp[i][w] = dp[i - 1][w];
+                }
+            }
+        }
+
+        // The maximum value will be in the bottom-right cell of the array
+        return dp[n][maximumWeight];
+    }
 }
