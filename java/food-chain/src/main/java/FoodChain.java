5775df6ae41a5b8ec9071291be8485d604c38bcd
Irfan Salim <irfansalim77@gmail.com>
2024-10-12 23:08:46 +0530

    Add  M java/food-chain/gradlew  M java/food-chain/src/main/java/FoodChain.java

--- java/food-chain/src/main/java/FoodChain.java
+++ java/food-chain/src/main/java/FoodChain.java
@@ -1,11 +1,44 @@
 class FoodChain {
 
+    private final static int NUMBER_MAX = 8;
+    private final static String[] animals = { "fly", "spider", "bird", "cat", "dog", "goat", "cow", "horse" };
+    private final static String[] phrase = { "I don't know why she swallowed the fly. Perhaps she'll die.",
+            "It wriggled and jiggled and tickled inside her.", "How absurd to swallow a bird!",
+            "Imagine that, to swallow a cat!", "What a hog, to swallow a dog!",
+            "Just opened her throat and swallowed a goat!", "I don't know how she swallowed a cow!",
+            "She's dead, of course!" };
+
     String verse(int verse) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        StringBuilder couplet = new StringBuilder("");
+        if (verse > 0) {
+            couplet.append("I know an old lady who swallowed a " + animals[verse - 1] + ".");
+            couplet.append("\n" + phrase[verse - 1]);
+            if (verse > 1 && verse < NUMBER_MAX) {
+                for (int i = 1; i < verse; i++) {
+                    if (verse - i - 1 == 1) {
+                        couplet.append("\n" + "She swallowed the " + animals[verse - i] +
+                                " to catch the " + animals[verse - i - 1] +
+                                " that wriggled and jiggled and tickled inside her.");
+                    } else {
+                        couplet.append("\n" + "She swallowed the " + animals[verse - i] +
+                                " to catch the " + animals[verse - i - 1] + ".");
+                    }
+
+                }
+                couplet.append("\n" + phrase[0]);
+            }
+        }
+        return couplet.toString();
     }
 
     String verses(int startVerse, int endVerse) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        StringBuilder song = new StringBuilder("");
+        for (int i = startVerse; i <= endVerse; i++) {
+            if (i != endVerse) {
+                song.append(verse(i) + "\n\n");
+            } else
+                song.append(verse(i));
+        }
+        return song.toString();
     }
-
 }
