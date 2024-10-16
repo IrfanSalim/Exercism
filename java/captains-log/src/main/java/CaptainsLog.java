75cd3da391fb89a421b80b4f5e54cb52ce506654
Irfan Salim <irfansalim77@gmail.com>
2024-10-16 23:35:58 +0530

    Add  M java/captains-log/gradlew  M java/captains-log/src/main/java/CaptainsLog.java

--- java/captains-log/src/main/java/CaptainsLog.java
+++ java/captains-log/src/main/java/CaptainsLog.java
@@ -2,7 +2,7 @@ import java.util.Random;
 
 class CaptainsLog {
 
-    private static final char[] PLANET_CLASSES = new char[]{'D', 'H', 'J', 'K', 'L', 'M', 'N', 'R', 'T', 'Y'};
+    private static final char[] PLANET_CLASSES = new char[] { 'D', 'H', 'J', 'K', 'L', 'M', 'N', 'R', 'T', 'Y' };
 
     private Random random;
 
@@ -11,14 +11,14 @@ class CaptainsLog {
     }
 
     char randomPlanetClass() {
-        throw new UnsupportedOperationException("Please implement the CaptainsLog.randomPlanetClass() method");
+        return PLANET_CLASSES[random.nextInt(PLANET_CLASSES.length)];
     }
 
     String randomShipRegistryNumber() {
-        throw new UnsupportedOperationException("Please implement the CaptainsLog.randomShipRegistryNumber() method");
+        return String.format("NCC-%d", 1000 + random.nextInt(9000));
     }
 
     double randomStardate() {
-        throw new UnsupportedOperationException("Please implement the CaptainsLog.randomStardate() method");
+        return 41000 + 1000 * random.nextDouble();
     }
 }
