eb3ccbde6bad0798dc90bbe5e966c1df9a0490bb
Irfan Salim <irfansalim77@gmail.com>
2024-10-19 23:56:20 +0530

    Add  M java/robot-name/gradlew  M java/robot-name/src/main/java/Robot.java

--- java/robot-name/src/main/java/Robot.java
+++ java/robot-name/src/main/java/Robot.java
@@ -1,11 +1,41 @@
-class Robot {
+import java.util.*;
 
-    String getName() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+public class Robot {
+    private static char[] LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+    private static Stack<String> unusedNames = generateNames();
+
+    private static Stack<String> generateNames() {
+        List<String> allNames = new ArrayList<>();
+
+        for (char firstLetter : LETTERS) {
+            for (char secondLetter : LETTERS) {
+                for (int i = 0; i < 1000; i++) {
+                    allNames.add(firstLetter + "" + secondLetter + String.format("%03d", i));
+                }
+            }
+        }
+
+        Collections.shuffle(allNames);
+        Stack<String> shuffledNames = new Stack<>();
+        shuffledNames.addAll(allNames);
+        return shuffledNames;
     }
 
-    void reset() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    private String name;
+
+    public Robot() {
+        reset();
     }
 
+    public void reset() throws EmptyStackException {
+        this.name = unusedNames.pop();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static void resetNames() {
+        unusedNames = generateNames();
+    }
 }
