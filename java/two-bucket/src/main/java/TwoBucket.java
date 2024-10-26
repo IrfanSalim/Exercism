954e55fcd76792bb0a17f4e1824c712eceeb2caa
Irfan Salim <irfansalim77@gmail.com>
2024-10-26 23:57:00 +0530

    Add  M java/two-bucket/src/main/java/TwoBucket.java

--- java/two-bucket/src/main/java/TwoBucket.java
+++ java/two-bucket/src/main/java/TwoBucket.java
@@ -1,17 +1,74 @@
 class TwoBucket {
-    TwoBucket(int bucketOneCap, int bucketTwoCap, int desiredLiters, String startBucket) {
-        throw new UnsupportedOperationException("Please implement the TwoBucket(int, int, int, String) constructor.");
+    private class Bucket {
+        final int bucketSize;
+        final String name;
+        int liters;
+
+        public Bucket(int bucketSize, String name) {
+            this.name = name;
+            this.bucketSize = bucketSize;
+            this.liters = 0;
+        }
     }
 
-    int getTotalMoves() {
-        throw new UnsupportedOperationException("Please implement the TwoBucket.getTotalMoves() method.");
+    private int moves = 0;
+    private int desired;
+    private Bucket firstBucket, secondBucket;
+
+    // constructor
+    public TwoBucket(int oneSize, int twoSize, int desired, String startBucket) {
+        this.desired = desired;
+        if (startBucket.equals("one")) {
+            firstBucket = new Bucket(oneSize, "one");
+            secondBucket = new Bucket(twoSize, "two");
+        } else {
+            firstBucket = new Bucket(twoSize, "two");
+            secondBucket = new Bucket(oneSize, "one");
+        }
+        calculateTwoBuckets();
     }
 
-    String getFinalBucket() {
-        throw new UnsupportedOperationException("Please implement the TwoBucket.getFinalBucket() method.");
+    private void calculateTwoBuckets() {
+        if (secondBucket.bucketSize == desired) {
+            fill();
+            secondBucket.liters = secondBucket.bucketSize;
+            moves += 2;
+        }
+        while (firstBucket.liters != desired && secondBucket.liters != desired) {
+            if (firstBucket.liters == 0)
+                fill();
+            else if (secondBucket.liters == secondBucket.bucketSize)
+                empty();
+            else
+                pour();
+            moves++;
+        } // end while loop
     }
 
-    int getOtherBucket() {
-        throw new UnsupportedOperationException("Please implement the TwoBucket.getOtherBucket() method.");
+    private void pour() {
+        int canTake = secondBucket.bucketSize - secondBucket.liters;
+        int pouringLiters = Math.min(firstBucket.liters, canTake);
+        firstBucket.liters -= pouringLiters;
+        secondBucket.liters += pouringLiters;
+    }
+
+    private void fill() {
+        firstBucket.liters = firstBucket.bucketSize;
+    }
+
+    private void empty() {
+        secondBucket.liters = 0;
+    }
+
+    public int getTotalMoves() {
+        return moves;
+    }
+
+    public String getFinalBucket() {
+        return firstBucket.liters == desired ? firstBucket.name : secondBucket.name;
+    }
+
+    public int getOtherBucket() {
+        return firstBucket.liters == desired ? secondBucket.liters : firstBucket.liters;
     }
 }
