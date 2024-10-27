64802ecdb96eebde9a9fd30d02d8750fb3c2b94c
Irfan Salim <irfansalim77@gmail.com>
2024-10-27 18:47:17 +0530

    Add  M java/rotational-cipher/gradlew  M java/rotational-cipher/src/main/java/RotationalCipher.java

--- java/rotational-cipher/src/main/java/RotationalCipher.java
+++ java/rotational-cipher/src/main/java/RotationalCipher.java
@@ -1,23 +1,48 @@
+// class RotationalCipher {
+//     private final int shiftKey;
+
+//     RotationalCipher(int shiftKey) {
+//         this.shiftKey = shiftKey;
+//     }
+
+//     String rotate(String data) {
+//         StringBuilder result = new StringBuilder();
+
+//         for (char ch : data.toCharArray()) {
+//             if (Character.isLetter(ch)) {
+//                 char base = Character.isUpperCase(ch) ? 'A' : 'a';
+//                 int shifted = (ch - base + shiftKey) % 26 + base;
+//                 result.append((char) shifted);
+//             } else {
+//                 result.append(ch);
+//             }
+//         }
+
+//         return result.toString();
+//     }
+// }
+
 class RotationalCipher {
-    private final int shiftKey;
+
+    private int shiftKey;
 
     RotationalCipher(int shiftKey) {
         this.shiftKey = shiftKey;
     }
 
-    String rotate(String data) {
-        StringBuilder result = new StringBuilder();
-
-        for (char ch : data.toCharArray()) {
-            if (Character.isLetter(ch)) {
-                char base = Character.isUpperCase(ch) ? 'A' : 'a';
-                int shifted = (ch - base + shiftKey) % 26 + base;
-                result.append((char) shifted);
-            } else {
-                result.append(ch);
-            }
+    int rot(int ch) {
+        if (!Character.isLetter(ch)) {
+            return ch;
         }
-
-        return result.toString();
+        char first = Character.isUpperCase(ch) ? 'A' : 'a';
+        return first + (ch + shiftKey - first) % 26;
     }
+
+    String rotate(String data) {
+        return data.codePoints()
+                .mapToObj(this::rot)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+    }
+
 }
