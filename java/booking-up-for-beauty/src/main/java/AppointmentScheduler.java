8c275313809473c7cfba2aab3316de65ca5efcfa
Irfan Salim <irfansalim77@gmail.com>
2024-10-28 16:45:35 +0530

    Add  M java/booking-up-for-beauty/gradlew  M java/booking-up-for-beauty/src/main/java/AppointmentScheduler.java

--- java/booking-up-for-beauty/src/main/java/AppointmentScheduler.java
+++ java/booking-up-for-beauty/src/main/java/AppointmentScheduler.java
@@ -1,24 +1,32 @@
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
+import java.util.Locale;
 
 class AppointmentScheduler {
     public LocalDateTime schedule(String appointmentDateDescription) {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.schedule() method");
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss");
+        return LocalDateTime.parse(appointmentDateDescription, dateFormatter);
     }
 
     public boolean hasPassed(LocalDateTime appointmentDate) {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.hasPassed() method");
+        return appointmentDate.isBefore(LocalDateTime.now());
     }
 
     public boolean isAfternoonAppointment(LocalDateTime appointmentDate) {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.isAfternoonAppointment() method");
+        return appointmentDate.getHour() >= 12 && appointmentDate.getHour() < 18;
     }
 
     public String getDescription(LocalDateTime appointmentDate) {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.getDescription() method");
+        return String.format("You have an appointment on %s, %s %s, %d, at %s.",
+                appointmentDate.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.ENGLISH),
+                appointmentDate.getMonth().getDisplayName(TextStyle.FULL, Locale.ENGLISH),
+                appointmentDate.getDayOfMonth(), appointmentDate.getYear(),
+                appointmentDate.format(DateTimeFormatter.ofPattern("h:mm a", Locale.ENGLISH)));
     }
 
     public LocalDate getAnniversaryDate() {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.getAnniversaryDate() method");
+        return LocalDate.of(2024, 9, 15);
     }
 }
