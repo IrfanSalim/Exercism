2b31e5f555fcedefdb01f1082b4537442f82de96
Irfan Salim <irfansalim77@gmail.com>
2024-10-30 17:30:43 +0530

    Add  M java/rail-fence-cipher/src/test/java/RailFenceCipherTest.java

--- java/rail-fence-cipher/src/test/java/RailFenceCipherTest.java
+++ java/rail-fence-cipher/src/test/java/RailFenceCipherTest.java
@@ -11,46 +11,41 @@ public class RailFenceCipherTest {
     public void encodeWithTwoRails() {
         railFenceCipher = new RailFenceCipher(2);
         assertThat(railFenceCipher.getEncryptedData("XOXOXOXOXOXOXOXOXO"))
-            .isEqualTo("XXXXXXXXXOOOOOOOOO");
+                .isEqualTo("XXXXXXXXXOOOOOOOOO");
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void encodeWithThreeRails() {
         railFenceCipher = new RailFenceCipher(3);
         assertThat(railFenceCipher.getEncryptedData("WEAREDISCOVEREDFLEEATONCE"))
-            .isEqualTo("WECRLTEERDSOEEFEAOCAIVDEN");
+                .isEqualTo("WECRLTEERDSOEEFEAOCAIVDEN");
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void encodeWithEndingInTheMiddle() {
         railFenceCipher = new RailFenceCipher(4);
         assertThat(railFenceCipher.getEncryptedData("EXERCISES"))
-            .isEqualTo("ESXIEECSR");
+                .isEqualTo("ESXIEECSR");
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void decodeWithThreeRails() {
         railFenceCipher = new RailFenceCipher(3);
         assertThat(railFenceCipher.getDecryptedData("TEITELHDVLSNHDTISEIIEA"))
-            .isEqualTo("THEDEVILISINTHEDETAILS");
+                .isEqualTo("THEDEVILISINTHEDETAILS");
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void decodeWithFiveRails() {
         railFenceCipher = new RailFenceCipher(5);
         assertThat(railFenceCipher.getDecryptedData("EIEXMSMESAORIWSCE"))
-            .isEqualTo("EXERCISMISAWESOME");
+                .isEqualTo("EXERCISMISAWESOME");
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void decodeWithSixRails() {
         railFenceCipher = new RailFenceCipher(6);
         assertThat(railFenceCipher.getDecryptedData("133714114238148966225439541018335470986172518171757571896261"))
-            .isEqualTo("112358132134558914423337761098715972584418167651094617711286");
+                .isEqualTo("112358132134558914423337761098715972584418167651094617711286");
     }
 }
