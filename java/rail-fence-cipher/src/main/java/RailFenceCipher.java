fb716c522323fa5c7c7a278e69d19e35ec9b3cf1
Irfan Salim <irfansalim77@gmail.com>
2024-10-31 20:00:45 +0530

    Add  M java/rail-fence-cipher/gradlew  M java/rail-fence-cipher/src/main/java/RailFenceCipher.java

--- java/rail-fence-cipher/src/main/java/RailFenceCipher.java
+++ java/rail-fence-cipher/src/main/java/RailFenceCipher.java
@@ -1,15 +1,71 @@
-class RailFenceCipher {
+public class RailFenceCipher {
 
-    RailFenceCipher(int rows) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    private int rails;
+
+    public RailFenceCipher(int rails) {
+        this.rails = rails;
     }
 
-    String getEncryptedData(String message) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    public String getEncryptedData(String input) {
+
+        char[][] encoded = makeRails(input);
+        StringBuilder encrypted = new StringBuilder();
+        for (char[] encodedRow : encoded) {
+            for (char encodedVal : encodedRow) {
+                if (encodedVal != '\u0000') {
+                    encrypted.append(encodedVal);
+                }
+            }
+        }
+
+        return encrypted.toString();
+
     }
 
-    String getDecryptedData(String message) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    public String getDecryptedData(String input) {
+        char[][] railed = makeRails(input);
+        StringBuilder decrypted = new StringBuilder();
+        int y = 0;
+        for (int i = 0; i < rails; i++) {
+            for (int x = 0; x < input.length(); x++) {
+                if (railed[i][x] != '\u0000') {
+                    railed[i][x] = input.charAt(y++);
+                }
+            }
+        }
+
+        boolean down = true;
+        for (int i = 0, x = 0; i < input.length(); i++) {
+
+            decrypted.append(railed[x][i]);
+
+            if (x == rails - 1) {
+                down = false;
+            } else if (x == 0) {
+                down = true;
+            }
+            x = down ? x + 1 : x - 1;
+        }
+
+        return decrypted.toString();
+
     }
 
+    private char[][] makeRails(String input) {
+        char[][] encoded = new char[rails][input.length()];
+        boolean down = true;
+        for (int i = 0, x = 0, y = 0; i < input.length(); i++) {
+
+            encoded[x][i] = input.charAt(i);
+
+            if (x == rails - 1) {
+                down = false;
+            } else if (x == 0) {
+                down = true;
+            }
+            x = down ? x + 1 : x - 1;
+        }
+
+        return encoded;
+    }
 }
