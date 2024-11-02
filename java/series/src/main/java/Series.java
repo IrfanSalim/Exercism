279190e65726afe28fbabea170b323a0db19b614
Irfan Salim <irfansalim77@gmail.com>
2024-11-02 23:04:43 +0530

    Add  M java/series/src/main/java/Series.java

--- java/series/src/main/java/Series.java
+++ java/series/src/main/java/Series.java
@@ -1,5 +1,36 @@
+// import java.util.ArrayList;
+// import java.util.List;
+
+// public class Series {
+//     private final String input;
+
+//     public Series(String input) {
+//         if (input == null || input.isEmpty()) {
+//             throw new IllegalArgumentException("series cannot be empty");
+//         }
+//         this.input = input;
+//     }
+
+//     public List<String> slices(int sliceLength) {
+//         if (sliceLength <= 0) {
+//             throw new IllegalArgumentException("slice length cannot be negative or zero");
+//         }
+//         if (sliceLength > input.length()) {
+//             throw new IllegalArgumentException("slice length cannot be greater than series length");
+//         }
+
+//         List<String> result = new ArrayList<>();
+//         for (int i = 0; i <= input.length() - sliceLength; i++) {
+//             result.add(input.substring(i, i + sliceLength));
+//         }
+//         return result;
+//     }
+// }
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Series {
     private final String input;
@@ -19,10 +50,9 @@ public class Series {
             throw new IllegalArgumentException("slice length cannot be greater than series length");
         }
 
-        List<String> result = new ArrayList<>();
-        for (int i = 0; i <= input.length() - sliceLength; i++) {
-            result.add(input.substring(i, i + sliceLength));
-        }
-        return result;
+        return IntStream
+                .rangeClosed(0, input.length() - sliceLength)
+                .mapToObj(i -> input.substring(i, i + sliceLength))
+                .collect(Collectors.toList());
     }
 }
