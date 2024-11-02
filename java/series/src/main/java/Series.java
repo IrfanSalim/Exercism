fa86fe7de08f038e305ed525f73424fcc6800bbc
Irfan Salim <irfansalim77@gmail.com>
2024-11-02 23:00:33 +0530

    Add  M java/series/gradlew  M java/series/src/main/java/Series.java

--- java/series/src/main/java/Series.java
+++ java/series/src/main/java/Series.java
@@ -1,11 +1,28 @@
+import java.util.ArrayList;
 import java.util.List;
 
-class Series {
-    Series(String string) {
-        throw new UnsupportedOperationException("Please implement the Series(string) constructor.");
+public class Series {
+    private final String input;
+
+    public Series(String input) {
+        if (input == null || input.isEmpty()) {
+            throw new IllegalArgumentException("series cannot be empty");
+        }
+        this.input = input;
     }
 
-    List<String> slices(int num) {
-        throw new UnsupportedOperationException("Please implement the Series.slices() method.");
+    public List<String> slices(int sliceLength) {
+        if (sliceLength <= 0) {
+            throw new IllegalArgumentException("slice length cannot be negative or zero");
+        }
+        if (sliceLength > input.length()) {
+            throw new IllegalArgumentException("slice length cannot be greater than series length");
+        }
+
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i <= input.length() - sliceLength; i++) {
+            result.add(input.substring(i, i + sliceLength));
+        }
+        return result;
     }
 }
