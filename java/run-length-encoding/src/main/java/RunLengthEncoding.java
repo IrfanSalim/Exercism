d0672a2b68560bf616003014d1cd2876bd6dbc37
Irfan Salim <irfansalim77@gmail.com>
2024-11-08 18:53:10 +0530

    Add  M java/run-length-encoding/gradlew  M java/run-length-encoding/src/main/java/RunLengthEncoding.java

--- java/run-length-encoding/src/main/java/RunLengthEncoding.java
+++ java/run-length-encoding/src/main/java/RunLengthEncoding.java
@@ -1,11 +1,52 @@
-class RunLengthEncoding {
+import java.util.regex.*;
 
-    String encode(String data) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+public class RunLengthEncoding {
+    public String encode(String text) {
+        StringBuilder builder = new StringBuilder();
+
+        if (text.length() == 0) {
+            return "";
+        }
+
+        char current = text.charAt(0);
+        int group = 1;
+
+        for (int i = 1; i < text.length(); i++) {
+            if (text.charAt(i) != current) {
+                if (group > 1) {
+                    builder.append(group);
+                }
+                builder.append(current);
+                current = text.charAt(i);
+                group = 1;
+            } else {
+                group++;
+            }
+
+        }
+        if (group > 1) {
+            builder.append(group);
+        }
+        builder.append(current);
+
+        return builder.toString();
     }
 
-    String decode(String data) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+    public String decode(String text) {
+        StringBuilder builder = new StringBuilder();
+        Matcher m = Pattern.compile("(\\d*)(\\D)").matcher(text);
+        while (m.find()) {
+            if (!m.group(1).isEmpty()) {
+                int size = Integer.parseInt(m.group(1));
+                char c = m.group(2).charAt(0);
+                for (int i = 0; i < size; i++) {
+                    builder.append(c);
+                }
+            } else {
+                builder.append(m.group());
+            }
+        }
 
+        return builder.toString();
+    }
 }
