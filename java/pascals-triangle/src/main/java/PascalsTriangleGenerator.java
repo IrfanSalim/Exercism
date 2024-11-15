84b92b1c747ed8d5306b6ac72e4ce3c151817b1a
Irfan Salim <irfansalim77@gmail.com>
2024-11-15 15:43:06 +0530

    Add  M java/pascals-triangle/src/main/java/PascalsTriangleGenerator.java

--- java/pascals-triangle/src/main/java/PascalsTriangleGenerator.java
+++ java/pascals-triangle/src/main/java/PascalsTriangleGenerator.java
@@ -1,15 +1,36 @@
-class PascalsTriangleGenerator {
+// class PascalsTriangleGenerator {
 
-    int[][] generateTriangle(int rows) {
-        int[][] triangle = new int[rows][];
-        for (int r = 0; r < rows; r++) {
-            triangle[r] = new int[r + 1];
-            triangle[r][0] = 1;
-            triangle[r][r] = 1;
-            for (int e = 1; e < r; e++) {
-                triangle[r][e] = triangle[r - 1][e - 1] + triangle[r - 1][e];
-            }
-        }
-        return triangle;
+//     int[][] generateTriangle(int rows) {
+//         int[][] triangle = new int[rows][];
+//         for (int r = 0; r < rows; r++) {
+//             triangle[r] = new int[r + 1];
+//             triangle[r][0] = 1;
+//             triangle[r][r] = 1;
+//             for (int e = 1; e < r; e++) {
+//                 triangle[r][e] = triangle[r - 1][e - 1] + triangle[r - 1][e];
+//             }
+//         }
+//         return triangle;
+//     }
+// }
+
+import java.util.stream.IntStream;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class PascalsTriangleGenerator {
+    public static int[][] generateTriangle(int size) {
+        return Stream
+                .iterate(new int[] { 1 }, PascalsTriangleGenerator::nextRow)
+                .limit(size)
+                .toArray(int[][]::new);
     }
+
+    private static int[] nextRow(int[] row) {
+        return IntStream
+                .range(0, row.length + 1)
+                .map(i -> i > 0 && i < row.length ? row[i] + row[i - 1] : 1)
+                .toArray();
+    }
+
 }
