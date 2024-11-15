f9879cec589451c2b5069d6833d8eb5148a46fc5
Irfan Salim <irfansalim77@gmail.com>
2024-11-15 15:41:28 +0530

    Add  M java/pascals-triangle/gradlew  M java/pascals-triangle/src/main/java/PascalsTriangleGenerator.java

--- java/pascals-triangle/src/main/java/PascalsTriangleGenerator.java
+++ java/pascals-triangle/src/main/java/PascalsTriangleGenerator.java
@@ -1,7 +1,15 @@
 class PascalsTriangleGenerator {
 
     int[][] generateTriangle(int rows) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        int[][] triangle = new int[rows][];
+        for (int r = 0; r < rows; r++) {
+            triangle[r] = new int[r + 1];
+            triangle[r][0] = 1;
+            triangle[r][r] = 1;
+            for (int e = 1; e < r; e++) {
+                triangle[r][e] = triangle[r - 1][e - 1] + triangle[r - 1][e];
+            }
+        }
+        return triangle;
     }
-
 }
