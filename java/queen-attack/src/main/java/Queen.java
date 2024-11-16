2ca06080daa4b853bb01227f6e6f33db799a8793
Irfan Salim <irfansalim77@gmail.com>
2024-11-16 19:27:29 +0530

    Add  M java/queen-attack/gradlew  M java/queen-attack/src/main/java/Queen.java  M java/queen-attack/src/main/java/QueenAttackCalculator.java

--- java/queen-attack/src/main/java/Queen.java
+++ java/queen-attack/src/main/java/Queen.java
@@ -1,7 +1,17 @@
 class Queen {
+    public final int r, c;
 
     Queen(int row, int column) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (row < 0) {
+            throw new IllegalArgumentException("Queen position must have positive row.");
+        } else if (row >= 8) {
+            throw new IllegalArgumentException("Queen position must have row <= 7.");
+        } else if (column < 0) {
+            throw new IllegalArgumentException("Queen position must have positive column.");
+        } else if (column >= 8) {
+            throw new IllegalArgumentException("Queen position must have column <= 7.");
+        }
+        r = row;
+        c = column;
     }
-
 }
--- java/queen-attack/src/main/java/QueenAttackCalculator.java
+++ java/queen-attack/src/main/java/QueenAttackCalculator.java
@@ -1,11 +1,17 @@
 class QueenAttackCalculator {
+    private final Queen q1, q2;
 
     QueenAttackCalculator(Queen queen1, Queen queen2) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (queen1 == null || queen2 == null) {
+            throw new IllegalArgumentException("You must supply valid positions for both Queens.");
+        } else if (queen1.r == queen2.r && queen1.c == queen2.c) {
+            throw new IllegalArgumentException("Queens cannot occupy the same position.");
+        }
+        q1 = queen1;
+        q2 = queen2;
     }
 
     boolean canQueensAttackOneAnother() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return (q1.r == q2.r || q1.c == q2.c || Math.abs(q1.r - q2.r) == Math.abs(q1.c - q2.c));
     }
-
 }
