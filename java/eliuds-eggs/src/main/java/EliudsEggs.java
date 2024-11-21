76a4711839a4e538aa128ca4881907442b3a6513
Irfan Salim <irfansalim77@gmail.com>
2024-11-21 20:06:37 +0530

    Add  M java/eliuds-eggs/gradlew  M java/eliuds-eggs/src/main/java/EliudsEggs.java

--- java/eliuds-eggs/src/main/java/EliudsEggs.java
+++ java/eliuds-eggs/src/main/java/EliudsEggs.java
@@ -1,5 +1,5 @@
 public class EliudsEggs {
     public int eggCount(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return Integer.bitCount(number);
     }
 }
