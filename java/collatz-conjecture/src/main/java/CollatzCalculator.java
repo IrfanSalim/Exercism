b7b428ccc8835aaef8ffaad506dd304126ee3319
Irfan Salim <irfansalim77@gmail.com>
2024-11-24 22:24:06 +0530

    Add  M java/collatz-conjecture/gradlew  M java/collatz-conjecture/src/main/java/CollatzCalculator.java

--- java/collatz-conjecture/src/main/java/CollatzCalculator.java
+++ java/collatz-conjecture/src/main/java/CollatzCalculator.java
@@ -1,16 +1,29 @@
+// class CollatzCalculator {
+
+//     int computeStepCount(int start) {
+//         if (start <= 0) {
+//             throw new IllegalArgumentException("Only positive integers are allowed");
+//         }
+//         int steps = 0;
+
+//         while (start != 1) {
+//             start = (start % 2 == 0) ? start / 2 : start * 3 + 1;
+//             steps++;
+//         }
+//         return steps;
+//     }
+
+// }
+
+import java.util.stream.IntStream;
+
 class CollatzCalculator {
 
-    int computeStepCount(int start) {
-        if (start <= 0) {
+    long computeStepCount(int start) {
+        if (start < 1) {
             throw new IllegalArgumentException("Only positive integers are allowed");
         }
-        int steps = 0;
 
-        while (start != 1) {
-            start = (start % 2 == 0) ? start / 2 : start * 3 + 1;
-            steps++;
-        }
-        return steps;
+        return IntStream.iterate(start, num -> num != 1, num -> (num & 1) == 1 ? 3 * num + 1 : num >> 1).count();
     }
-
 }
