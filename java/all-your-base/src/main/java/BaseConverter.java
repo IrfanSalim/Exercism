6d7900da6a12038ae0b9041f167d2b0b4047b16d
Irfan Salim <irfansalim77@gmail.com>
2024-11-26 20:49:02 +0530

    Add  M java/all-your-base/src/main/java/BaseConverter.java

--- java/all-your-base/src/main/java/BaseConverter.java
+++ java/all-your-base/src/main/java/BaseConverter.java
@@ -1,11 +1,35 @@
-class BaseConverter {
+import java.util.*;
 
-    BaseConverter(int originalBase, int[] originalDigits) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+public class BaseConverter {
+    private int value = 0;
+
+    public BaseConverter(int base, int[] number) {
+        if (base < 2)
+            throw new IllegalArgumentException("Bases must be at least 2.");
+
+        int l = number.length;
+        for (int i = 0; i < l; i++) {
+            int v = number[l - i - 1];
+            if (v >= base)
+                throw new IllegalArgumentException("All digits must be strictly less than the base.");
+            if (v < 0)
+                throw new IllegalArgumentException("Digits may not be negative.");
+            value += v * (int) Math.pow(base, i);
+        }
     }
 
-    int[] convertToBase(int newBase) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    public List<Integer> convertToBase(int base) {
+        if (base < 2)
+            throw new IllegalArgumentException("Bases must be at least 2.");
+        if (value == 0)
+            return Arrays.asList(0);
+
+        LinkedList<Integer> result = new LinkedList<>();
+        while (value != 0) {
+            result.addFirst(value % base);
+            value /= base;
+        }
+        return result;
     }
 
 }
