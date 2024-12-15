fd33b90c0d4f7b59d1cfe2e22207d604c1883409
Irfan Salim <irfansalim77@gmail.com>
2024-12-15 23:15:08 +0530

    Add  M java/etl/gradlew  M java/etl/src/main/java/Etl.java

--- java/etl/src/main/java/Etl.java
+++ java/etl/src/main/java/Etl.java
@@ -1,8 +1,15 @@
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-class Etl {
-    Map<String, Integer> transform(Map<Integer, List<String>> old) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+public class Etl {
+    public Map<String, Integer> transform(Map<Integer, List<String>> old) {
+        Map<String, Integer> result = new HashMap<>();
+        for (Map.Entry<Integer, List<String>> entry : old.entrySet()) {
+            for (String letter : entry.getValue()) {
+                result.put(letter.toLowerCase(), entry.getKey());
+            }
+        }
+        return result;
     }
 }
