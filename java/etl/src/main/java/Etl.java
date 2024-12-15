048c755a4b7e8a9e778a59b2de6aa64a2ff783d2
Irfan Salim <irfansalim77@gmail.com>
2024-12-15 23:15:39 +0530

    Add  M java/etl/src/main/java/Etl.java

--- java/etl/src/main/java/Etl.java
+++ java/etl/src/main/java/Etl.java
@@ -1,15 +1,33 @@
-import java.util.HashMap;
+// import java.util.HashMap;
+// import java.util.List;
+// import java.util.Map;
+
+// public class Etl {
+//     public Map<String, Integer> transform(Map<Integer, List<String>> old) {
+//         Map<String, Integer> result = new HashMap<>();
+//         for (Map.Entry<Integer, List<String>> entry : old.entrySet()) {
+//             for (String letter : entry.getValue()) {
+//                 result.put(letter.toLowerCase(), entry.getKey());
+//             }
+//         }
+//         return result;
+//     }
+// }
+
+import java.util.AbstractMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
-public class Etl {
-    public Map<String, Integer> transform(Map<Integer, List<String>> old) {
-        Map<String, Integer> result = new HashMap<>();
-        for (Map.Entry<Integer, List<String>> entry : old.entrySet()) {
-            for (String letter : entry.getValue()) {
-                result.put(letter.toLowerCase(), entry.getKey());
-            }
-        }
-        return result;
+class Etl {
+    Map<String, Integer> transform(Map<Integer, List<String>> old) {
+
+        return old.entrySet()
+                .stream()
+                .flatMap(entry -> entry.getValue()
+                        .stream()
+                        .map(s -> new AbstractMap.SimpleEntry<>(s.toLowerCase(), entry.getKey())))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
     }
 }
