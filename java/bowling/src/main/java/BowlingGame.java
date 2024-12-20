51e2a5a2ff0c062f3d887a26f9d032e07aa64e52
Irfan Salim <irfansalim77@gmail.com>
2024-12-20 21:30:28 +0530

    Add  M java/bowling/gradlew  M java/bowling/src/main/java/BowlingGame.java

--- java/bowling/src/main/java/BowlingGame.java
+++ java/bowling/src/main/java/BowlingGame.java
@@ -1,11 +1,53 @@
 class BowlingGame {
+    private static int MAX_PINS = 10;
+    private static int LAST_FRAME = 10;
+
+    private int frames = 0;
+    private int score = 0;
+    private Integer pendingPins = null;
+    private int countNext1 = 0;
+    private int countNext2 = 0;
 
     void roll(int pins) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (finished())
+            throw new IllegalStateException("Cannot roll after game is over");
+        if (pins < 0)
+            throw new IllegalStateException("Negative roll is invalid");
+        if ((pendingPins == null ? pins : pendingPins + pins) > MAX_PINS)
+            throw new IllegalStateException("Pin count exceeds pins on the lane");
+        if (countNext1 > 0) {
+            score += pins * countNext1;
+            countNext1 = countNext2;
+            countNext2 = 0;
+        }
+        if (frames == LAST_FRAME) {
+            if (pins != MAX_PINS)
+                pendingPins = pins;
+            return;
+        }
+        score += pins;
+        if (pendingPins != null) {
+            if (pendingPins + pins == MAX_PINS)
+                countNext1++;
+            pendingPins = null;
+            frames++;
+        } else if (pins == MAX_PINS) {
+            countNext1++;
+            countNext2++;
+            frames++;
+        } else {
+            pendingPins = pins;
+        }
     }
 
     int score() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (!finished())
+            throw new IllegalStateException("Score cannot be taken until the end of the game");
+        return score;
+
     }
 
+    private boolean finished() {
+        return frames == LAST_FRAME && countNext1 == 0;
+    }
 }
