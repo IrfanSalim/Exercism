16f98a021b74b3c8533054d09b5cda70710a01e1
Irfan Salim <irfansalim77@gmail.com>
2024-12-24 15:29:06 +0530

    Add  M java/crypto-square/gradlew  M java/crypto-square/src/main/java/CryptoSquare.java

--- java/crypto-square/src/main/java/CryptoSquare.java
+++ java/crypto-square/src/main/java/CryptoSquare.java
@@ -1,11 +1,41 @@
 class CryptoSquare {
 
-    CryptoSquare(String plaintext) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    private StringBuilder text;
+    private int size = 0, columns = 1;
+
+    CryptoSquare(String s) {
+        text = new StringBuilder();
+
+        for (char c : s.toLowerCase().toCharArray())
+            if (Character.isLetterOrDigit(c)) {
+                text.append(c);
+                ++size;
+            }
+
+        if (size < 2)
+            return;
+
+        columns = (int) Math.ceil(Math.sqrt(size));
+
+        while (size % columns != 0) {
+            text.append(' ');
+            ++size;
+        }
     }
 
     String getCiphertext() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+        if (size < 2)
+            return text.toString();
 
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < columns; i++) {
+            int pos = i;
+            while (pos < size) {
+                res.append(text.charAt(pos));
+                pos += columns;
+            }
+            res.append(' ');
+        }
+        return res.substring(0, res.length() - 1).toString();
+    }
 }
