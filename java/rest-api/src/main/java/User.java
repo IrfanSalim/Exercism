f017244963dcfef54cd1556d2582ce85fb96b494
Irfan Salim <irfansalim77@gmail.com>
2024-12-26 11:34:42 +0530

    Add  M java/rest-api/src/main/java/User.java

--- java/rest-api/src/main/java/User.java
+++ java/rest-api/src/main/java/User.java
@@ -1,7 +1,8 @@
 import static java.util.Collections.unmodifiableList;
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Iterator;
+import java.util.function.BiFunction;
 
 /** POJO representing a User in the database. */
 public class User {
@@ -27,6 +28,42 @@ public class User {
     /** IOUs other users owe to this user. */
     public List<Iou> owedBy() {
         return unmodifiableList(owedBy);
+    }
+
+    public double balance() {
+        double balance = 0.0;
+        for (final Iou iou : owes)
+            balance -= iou.amount;
+        for (final Iou iou : owedBy)
+            balance += iou.amount;
+        return balance;
+    }
+
+    public void updateOwes(final String name, final double amount) {
+        final double newAmount = removeIousAndAdjustAmount(name, amount);
+        if (newAmount < 0)
+            owedBy.add(new Iou(name, -newAmount));
+        else if (newAmount != 0)
+            owes.add(new Iou(name, newAmount));
+    }
+
+    private double removeIousAndAdjustAmount(final String name, final double amount) {
+        double newAmount = amount;
+        newAmount = dropIous(owedBy, name, (a, iou) -> a - iou.amount, newAmount);
+        newAmount = dropIous(owes, name, (a, iou) -> a + iou.amount, newAmount);
+        return newAmount;
+    }
+
+    private <T> T dropIous(final List<Iou> ious, final String name, final BiFunction<T, Iou, T> callback, T init) {
+        final Iterator<Iou> iter = ious.iterator();
+        while (iter.hasNext()) {
+            final Iou iou = iter.next();
+            if (!iou.name.equals(name))
+                continue;
+            init = callback.apply(init, iou);
+            iter.remove();
+        }
+        return init;
     }
 
     public static Builder builder() {
