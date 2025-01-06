0f97962eebdea46d2b993e70e35992480674e8ac
Irfan Salim <irfansalim77@gmail.com>
2025-01-06 23:58:53 +0530

    Add  M java/logs-logs-logs/gradlew  M java/logs-logs-logs/src/main/java/LogLevel.java  M java/logs-logs-logs/src/main/java/LogLine.java

--- java/logs-logs-logs/src/main/java/LogLevel.java
+++ java/logs-logs-logs/src/main/java/LogLevel.java
@@ -1,3 +1,20 @@
 public enum LogLevel {
-    // TODO: define members for each log level
+    TRACE(1),
+    DEBUG(2),
+    INFO(4),
+    WARNING(5),
+    ERROR(6),
+    FATAL(42),
+    UNKNOWN(0);
+
+    private final Integer typeLog;
+
+    LogLevel(Integer typeLog) {
+        this.typeLog = typeLog;
+    }
+
+    public Integer getTypeLog() {
+        return this.typeLog;
+    }
+
 }
--- java/logs-logs-logs/src/main/java/LogLine.java
+++ java/logs-logs-logs/src/main/java/LogLine.java
@@ -1,13 +1,33 @@
 public class LogLine {
+    String logLine;
 
     public LogLine(String logLine) {
+        this.logLine = logLine;
     }
 
     public LogLevel getLogLevel() {
-        throw new UnsupportedOperationException("Please implement the getLogLevel() method");
+        String strLog = this.logLine.substring(1, 4);
+        switch (strLog) {
+            case "TRC":
+                return LogLevel.TRACE;
+            case "DBG":
+                return LogLevel.DEBUG;
+            case "INF":
+                return LogLevel.INFO;
+            case "WRN":
+                return LogLevel.WARNING;
+            case "ERR":
+                return LogLevel.ERROR;
+            case "FTL":
+                return LogLevel.FATAL;
+            default:
+                return LogLevel.UNKNOWN;
+        }
     }
 
     public String getOutputForShortLog() {
-        throw new UnsupportedOperationException("Please implement the getOutputForShortLog() method");
+        String strLog = this.logLine.substring(7);
+        LogLevel logLvlType = this.getLogLevel();
+        return logLvlType.getTypeLog() + ":" + String.valueOf(strLog);
     }
 }
