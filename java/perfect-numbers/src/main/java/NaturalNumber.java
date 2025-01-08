e713ef0b748602d840a8a55b8d078b8c1ed89a85
Irfan Salim <irfansalim77@gmail.com>
2025-01-08 20:31:10 +0530

    Add  M java/perfect-numbers/gradlew  M java/perfect-numbers/src/main/java/NaturalNumber.java

--- java/perfect-numbers/src/main/java/NaturalNumber.java
+++ java/perfect-numbers/src/main/java/NaturalNumber.java
@@ -1,9 +1,29 @@
+import java.util.stream.IntStream;
+
 class NaturalNumber {
+
+    int number;
+
     NaturalNumber(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation");
+        if (number <= 0) {
+            throw new IllegalArgumentException("You must supply a natural number (positive integer)");
+        }
+
+        this.number = number;
     }
 
     Classification getClassification() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        int aliquotSum = IntStream.rangeClosed(1, this.number / 2).parallel()
+                .filter(i -> this.number % i == 0)
+                .sum();
+
+        if (aliquotSum < number) {
+            return Classification.DEFICIENT;
+        } else if (aliquotSum > number) {
+            return Classification.ABUNDANT;
+        } else {
+            return Classification.PERFECT;
+        }
     }
+
 }
