63666635b641aa1ded2b08a55bca9027e8e2a4e4
Irfan Salim <irfansalim77@gmail.com>
2025-01-11 12:27:03 +0530

    Add  M java/allergies/src/main/java/Allergies.java

--- java/allergies/src/main/java/Allergies.java
+++ java/allergies/src/main/java/Allergies.java
@@ -1,29 +1,51 @@
-import java.util.List;
+// import java.util.List;
+// import java.util.Arrays;
+// import java.util.ArrayList;
+
+// public class Allergies {
+
+//     private final int score;
+
+//     public Allergies(int score) {
+//         this.score = score;
+//     }
+
+//     public boolean isAllergicTo(Allergen a) {
+//         return getList().contains(a);
+//     }
+
+//     public List<Allergen> getList() {
+//         List<Allergen> results = new ArrayList<Allergen>();
+
+//         for (Allergen a : Allergen.values()) {
+//             if ((score & a.getScore()) != 0) {
+//                 results.add(a);
+//             }
+//         }
+
+//         return results;
+//     }
+
+// }
+
 import java.util.Arrays;
-import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Allergies {
-
-    private final int score;
+    private int score;
 
     public Allergies(int score) {
         this.score = score;
     }
 
-    public boolean isAllergicTo(Allergen a) {
-        return getList().contains(a);
+    public boolean isAllergicTo(Allergen allergen) {
+        return (score & allergen.getScore()) != 0;
     }
 
     public List<Allergen> getList() {
-        List<Allergen> results = new ArrayList<Allergen>();
-
-        for (Allergen a : Allergen.values()) {
-            if ((score & a.getScore()) != 0) {
-                results.add(a);
-            }
-        }
-
-        return results;
+        return Arrays.stream(Allergen.values())
+                .filter(this::isAllergicTo)
+                .collect(Collectors.toList());
     }
-
 }
