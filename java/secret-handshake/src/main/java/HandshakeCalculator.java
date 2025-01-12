254dccbceda54ebe622d591a7e4031f3eac0d4ae
Irfan Salim <irfansalim77@gmail.com>
2025-01-12 21:28:41 +0530

    Add  M java/secret-handshake/gradlew  M java/secret-handshake/src/main/java/HandshakeCalculator.java

--- java/secret-handshake/src/main/java/HandshakeCalculator.java
+++ java/secret-handshake/src/main/java/HandshakeCalculator.java
@@ -1,9 +1,23 @@
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 
 class HandshakeCalculator {
 
     List<Signal> calculateHandshake(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        List<Signal> handshakes = new ArrayList<Signal>();
+        if ((number & 1 << 0) != 0)
+            handshakes.add(Signal.WINK);
+        if ((number & 1 << 1) != 0)
+            handshakes.add(Signal.DOUBLE_BLINK);
+        if ((number & 1 << 2) != 0)
+            handshakes.add(Signal.CLOSE_YOUR_EYES);
+        if ((number & 1 << 3) != 0)
+            handshakes.add(Signal.JUMP);
+        if ((number & 1 << 4) != 0)
+            Collections.reverse(handshakes);
+
+        return handshakes;
     }
 
 }
