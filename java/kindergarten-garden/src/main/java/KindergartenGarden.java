289abbfe2e2d20e56c1f0bb4340a3154e99149b9
Irfan Salim <irfansalim77@gmail.com>
2025-01-14 23:36:45 +0530

    Add  M java/kindergarten-garden/src/main/java/KindergartenGarden.java

--- java/kindergarten-garden/src/main/java/KindergartenGarden.java
+++ java/kindergarten-garden/src/main/java/KindergartenGarden.java
@@ -1,39 +1,68 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
+// import java.util.ArrayList;
+// import java.util.Arrays;
+// import java.util.Collections;
+// import java.util.HashMap;
+// import java.util.List;
+// import java.util.Map;
+
+// public class KindergartenGarden {
+//     private final String[] rows;
+//     private List<String> students;
+
+//     private static String[] defaultStudents = new String[] { "Alice", "Bob", "Charlie", "David", "Eve", "Fred", "Ginny",
+//             "Harriet", "Ileana", "Joseph", "Kincaid", "Larry" };
+
+//     public KindergartenGarden(String plants) {
+//         this(plants, defaultStudents);
+//     }
+
+//     public KindergartenGarden(String plants, String[] students) {
+//         rows = plants.split("\\n");
+//         this.students = Arrays.asList(students);
+//         Collections.sort(this.students);
+//     }
+
+//     public List<Plant> getPlantsOfStudent(String student) {
+
+//         List<Plant> result = new ArrayList<Plant>();
+//         int i = students.indexOf(student);
+
+//         for (String row : rows) {
+//             result.add(Plant.getPlant(row.charAt(i * 2)));
+//             result.add(Plant.getPlant(row.charAt(i * 2 + 1)));
+//         }
+
+//         return result;
+
+//     }
+
+// }
+
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.toList;
+
 import java.util.List;
-import java.util.Map;
 
-public class KindergartenGarden {
-    private final String[] rows;
-    private List<String> students;
+class KindergartenGarden {
 
-    private static String[] defaultStudents = new String[] { "Alice", "Bob", "Charlie", "David", "Eve", "Fred", "Ginny",
-            "Harriet", "Ileana", "Joseph", "Kincaid", "Larry" };
+    private final List<List<Plant>> plants;
 
-    public KindergartenGarden(String plants) {
-        this(plants, defaultStudents);
+    KindergartenGarden(String garden) {
+        plants = stream(garden.split("\n"))
+                .map(this::parseRow)
+                .collect(toList());
     }
 
-    public KindergartenGarden(String plants, String[] students) {
-        rows = plants.split("\\n");
-        this.students = Arrays.asList(students);
-        Collections.sort(this.students);
+    private List<Plant> parseRow(String row) {
+        return row.codePoints()
+                .mapToObj(c -> Plant.getPlant((char) c))
+                .collect(toList());
     }
 
-    public List<Plant> getPlantsOfStudent(String student) {
-
-        List<Plant> result = new ArrayList<Plant>();
-        int i = students.indexOf(student);
-
-        for (String row : rows) {
-            result.add(Plant.getPlant(row.charAt(i * 2)));
-            result.add(Plant.getPlant(row.charAt(i * 2 + 1)));
-        }
-
-        return result;
-
+    List<Plant> getPlantsOfStudent(String student) {
+        final int studentIndex = student.charAt(0) - 'A';
+        return plants.stream()
+                .flatMap(row -> row.subList(studentIndex * 2, studentIndex * 2 + 2).stream())
+                .collect(toList());
     }
-
 }
