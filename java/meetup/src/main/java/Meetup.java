edfa65d3e2e3d7086acbc31ac2eef4336d2fdbde
Irfan Salim <irfansalim77@gmail.com>
2025-01-19 23:27:40 +0530

    Add  M java/meetup/gradlew  M java/meetup/src/main/java/Meetup.java

--- java/meetup/src/main/java/Meetup.java
+++ java/meetup/src/main/java/Meetup.java
@@ -1,14 +1,33 @@
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
 
 class Meetup {
+    private int monthOfYear;
+    private int year;
 
     Meetup(int monthOfYear, int year) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.monthOfYear = monthOfYear;
+        this.year = year;
     }
 
     LocalDate day(DayOfWeek dayOfWeek, MeetupSchedule schedule) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        LocalDate meetupDay = LocalDate.of(year, monthOfYear, 13);
+        switch (schedule) {
+            case FIRST:
+                return meetupDay.with(TemporalAdjusters.firstInMonth(dayOfWeek));
+            case SECOND:
+                return meetupDay.with(TemporalAdjusters.dayOfWeekInMonth(2, dayOfWeek));
+            case THIRD:
+                return meetupDay.with(TemporalAdjusters.dayOfWeekInMonth(3, dayOfWeek));
+            case FOURTH:
+                return meetupDay.with(TemporalAdjusters.dayOfWeekInMonth(4, dayOfWeek));
+            case LAST:
+                return meetupDay.with(TemporalAdjusters.lastInMonth(dayOfWeek));
+            case TEENTH:
+                return meetupDay.with(TemporalAdjusters.nextOrSame(dayOfWeek));
+            default:
+                return LocalDate.now();
+        }
     }
-
 }
