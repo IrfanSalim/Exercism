be3dc373199b22e44903692eb8a12c931fd5bd05
Irfan Salim <irfansalim77@gmail.com>
2025-01-19 23:28:41 +0530

    Add  M java/meetup/src/main/java/Meetup.java

--- java/meetup/src/main/java/Meetup.java
+++ java/meetup/src/main/java/Meetup.java
@@ -1,8 +1,42 @@
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.temporal.TemporalAdjusters;
+// import java.time.DayOfWeek;
+// import java.time.LocalDate;
+// import java.time.temporal.TemporalAdjusters;
+
+// class Meetup {
+//     private int monthOfYear;
+//     private int year;
+
+//     Meetup(int monthOfYear, int year) {
+//         this.monthOfYear = monthOfYear;
+//         this.year = year;
+//     }
+
+//     LocalDate day(DayOfWeek dayOfWeek, MeetupSchedule schedule) {
+//         LocalDate meetupDay = LocalDate.of(year, monthOfYear, 13);
+//         switch (schedule) {
+//             case FIRST:
+//                 return meetupDay.with(TemporalAdjusters.firstInMonth(dayOfWeek));
+//             case SECOND:
+//                 return meetupDay.with(TemporalAdjusters.dayOfWeekInMonth(2, dayOfWeek));
+//             case THIRD:
+//                 return meetupDay.with(TemporalAdjusters.dayOfWeekInMonth(3, dayOfWeek));
+//             case FOURTH:
+//                 return meetupDay.with(TemporalAdjusters.dayOfWeekInMonth(4, dayOfWeek));
+//             case LAST:
+//                 return meetupDay.with(TemporalAdjusters.lastInMonth(dayOfWeek));
+//             case TEENTH:
+//                 return meetupDay.with(TemporalAdjusters.nextOrSame(dayOfWeek));
+//             default:
+//                 return LocalDate.now();
+//         }
+//     }
+// }
+
+import java.time.*;
+import static java.time.temporal.TemporalAdjusters.*;
 
 class Meetup {
+
     private int monthOfYear;
     private int year;
 
@@ -12,22 +46,10 @@ class Meetup {
     }
 
     LocalDate day(DayOfWeek dayOfWeek, MeetupSchedule schedule) {
-        LocalDate meetupDay = LocalDate.of(year, monthOfYear, 13);
-        switch (schedule) {
-            case FIRST:
-                return meetupDay.with(TemporalAdjusters.firstInMonth(dayOfWeek));
-            case SECOND:
-                return meetupDay.with(TemporalAdjusters.dayOfWeekInMonth(2, dayOfWeek));
-            case THIRD:
-                return meetupDay.with(TemporalAdjusters.dayOfWeekInMonth(3, dayOfWeek));
-            case FOURTH:
-                return meetupDay.with(TemporalAdjusters.dayOfWeekInMonth(4, dayOfWeek));
-            case LAST:
-                return meetupDay.with(TemporalAdjusters.lastInMonth(dayOfWeek));
-            case TEENTH:
-                return meetupDay.with(TemporalAdjusters.nextOrSame(dayOfWeek));
-            default:
-                return LocalDate.now();
-        }
+        if (schedule == MeetupSchedule.TEENTH)
+            return LocalDate.of(year, monthOfYear, 12).with(next(dayOfWeek));
+        if (schedule == MeetupSchedule.LAST)
+            return LocalDate.of(year, monthOfYear, 1).with(lastDayOfMonth()).with(previousOrSame(dayOfWeek));
+        return LocalDate.of(year, monthOfYear, 1).with(dayOfWeekInMonth(schedule.ordinal() + 1, dayOfWeek));
     }
 }
