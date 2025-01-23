5275280d2850f29765d633e4d8f80886fe3ebf1a
Irfan Salim <irfansalim77@gmail.com>
2025-01-23 23:49:25 +0530

    Add  M java/sublist/gradlew  M java/sublist/src/main/java/RelationshipComputer.java

--- java/sublist/src/main/java/RelationshipComputer.java
+++ java/sublist/src/main/java/RelationshipComputer.java
@@ -1,7 +1,21 @@
+import java.util.Collections;
 import java.util.List;
 
-class RelationshipComputer<T> {
-    Relationship computeRelationship(List<T> firstList, List<T> secondList) {
-        throw new UnsupportedOperationException("Please implement the RelationshipComputer.computeRelationship() method.");
+public class RelationshipComputer<T> {
+
+    public Relationship computeRelationship(List<T> a, List<T> b) {
+        if (a.equals(b)) {
+            return Relationship.EQUAL;
+        }
+
+        if (Collections.indexOfSubList(a, b) != -1) {
+            return Relationship.SUPERLIST;
+        }
+
+        if (Collections.indexOfSubList(b, a) != -1) {
+            return Relationship.SUBLIST;
+        }
+
+        return Relationship.UNEQUAL;
     }
 }
