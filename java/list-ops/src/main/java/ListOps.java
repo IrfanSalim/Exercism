f9f002f2b9fbb0d54dc48a332eb75d73fe0d8c12
Irfan Salim <irfansalim77@gmail.com>
2025-01-25 23:57:19 +0530

    Add  M java/list-ops/gradlew  M java/list-ops/src/main/java/ListOps.java

--- java/list-ops/src/main/java/ListOps.java
+++ java/list-ops/src/main/java/ListOps.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BiFunction;
 import java.util.function.Function;
@@ -6,35 +7,61 @@ import java.util.function.Predicate;
 class ListOps {
 
     static <T> List<T> append(List<T> list1, List<T> list2) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        List<T> result = new ArrayList<>(list1);
+        result.addAll(list2);
+        return result;
     }
 
     static <T> List<T> concat(List<List<T>> listOfLists) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        List<T> result = new ArrayList<>();
+        for (List<T> list : listOfLists) {
+            result.addAll(list);
+        }
+        return result;
     }
 
     static <T> List<T> filter(List<T> list, Predicate<T> predicate) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        List<T> result = new ArrayList<>();
+        for (T item : list) {
+            if (predicate.test(item)) {
+                result.add(item);
+            }
+        }
+        return result;
     }
 
     static <T> int size(List<T> list) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return list.size();
     }
 
     static <T, U> List<U> map(List<T> list, Function<T, U> transform) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        List<U> result = new ArrayList<>();
+        for (T item : list) {
+            result.add(transform.apply(item));
+        }
+        return result;
     }
 
     static <T> List<T> reverse(List<T> list) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        List<T> result = new ArrayList<>();
+        for (int i = list.size() - 1; i >= 0; i--) {
+            result.add(list.get(i));
+        }
+        return result;
     }
 
     static <T, U> U foldLeft(List<T> list, U initial, BiFunction<U, T, U> f) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        for (T item : list) {
+            initial = f.apply(initial, item);
+        }
+        return initial;
     }
 
     static <T, U> U foldRight(List<T> list, U initial, BiFunction<T, U, U> f) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        for (int i = list.size() - 1; i >= 0; i--) {
+            initial = f.apply(list.get(i), initial);
+        }
+        return initial;
     }
 
     private ListOps() {
