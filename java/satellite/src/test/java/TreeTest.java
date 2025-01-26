4535aab2023344e05acc89c0eb79b5db9b96889c
Irfan Salim <irfansalim77@gmail.com>
2025-01-26 12:07:08 +0530

    Add  M java/satellite/src/test/java/SatelliteTest.java  M java/satellite/src/test/java/TreeTest.java

--- java/satellite/src/test/java/TreeTest.java
+++ java/satellite/src/test/java/TreeTest.java
@@ -7,38 +7,36 @@ public class TreeTest {
     @Test
     public void inorder() {
         Tree tree = new Tree(
-            new Node(
-                'C',
-                new Node('B', new Node('A'), null),
-                new Node('E', new Node('D'), new Node('F'))));
+                new Node(
+                        'C',
+                        new Node('B', new Node('A'), null),
+                        new Node('E', new Node('D'), new Node('F'))));
 
         assertThat(tree.inorder())
-            .containsExactly('A', 'B', 'C', 'D', 'E', 'F');
+                .containsExactly('A', 'B', 'C', 'D', 'E', 'F');
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void preorder() {
         Tree tree = new Tree(
-            new Node(
-                'A',
-                new Node('B', null, new Node('C')),
-                new Node('D', new Node('E'), new Node('F'))));
+                new Node(
+                        'A',
+                        new Node('B', null, new Node('C')),
+                        new Node('D', new Node('E'), new Node('F'))));
 
         assertThat(tree.preorder())
-            .containsExactly('A', 'B', 'C', 'D', 'E', 'F');
+                .containsExactly('A', 'B', 'C', 'D', 'E', 'F');
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void postorder() {
         Tree tree = new Tree(
-            new Node(
-                'F',
-                new Node('B', null, new Node('A')),
-                new Node('E', new Node('C'), new Node('D'))));
+                new Node(
+                        'F',
+                        new Node('B', null, new Node('A')),
+                        new Node('E', new Node('C'), new Node('D'))));
 
         assertThat(tree.postorder())
-            .containsExactly('A', 'B', 'C', 'D', 'E', 'F');
+                .containsExactly('A', 'B', 'C', 'D', 'E', 'F');
     }
 }
