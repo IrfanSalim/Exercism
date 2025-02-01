4cb639dfeccd8374c4487149b56fffac9272ffda
Irfan Salim <irfansalim77@gmail.com>
2025-02-01 19:53:28 +0530

    Add  M java/affine-cipher/gradlew  M java/affine-cipher/src/main/java/AffineCipher.java

--- java/affine-cipher/src/main/java/AffineCipher.java
+++ java/affine-cipher/src/main/java/AffineCipher.java
@@ -1,10 +1,48 @@
+import java.math.BigInteger;
+import java.util.stream.Collectors;
+
 public class AffineCipher {
-    
-    public String encode(String text, int coefficient1, int coefficient2){
-        throw new UnsupportedOperationException("Please implement AffineCipher.encode() method.");
+
+    private String ERROR = "Error: keyA and alphabet size must be coprime.";
+
+    private static int M = 26;
+    private static int φM = 12;
+
+    private static int ASCII_A = 'a';
+
+    public String encode(String text, int a, int b) {
+        if (!BigInteger.valueOf(a).gcd(BigInteger.valueOf(M)).equals(BigInteger.ONE)) {
+            throw new IllegalArgumentException(ERROR);
+        }
+
+        StringBuilder result = new StringBuilder();
+        var tail = text.toLowerCase().chars()
+                .filter(Character::isLetterOrDigit)
+                .map(c -> Character.isDigit(c) ? c : Math.floorMod(a * (c - ASCII_A) + b, M) + ASCII_A)
+                .mapToObj(Character::toString)
+                .reduce("", (str, nextChar) -> {
+                    if (str.length() == 5) {
+                        result.append(str);
+                        result.append(" ");
+                        str = "";
+                    }
+                    return str + nextChar;
+                });
+
+        return result.append(tail).toString();
+
     }
 
-    public String decode(String text, int coefficient1, int coefficient2){
-        throw new UnsupportedOperationException("Please implement AffineCipher.decode() method.");
+    public String decode(String cipher, int a, int b) {
+        if (!BigInteger.valueOf(a).gcd(BigInteger.valueOf(M)).equals(BigInteger.ONE)) {
+            throw new IllegalArgumentException(ERROR);
+        }
+
+        int inverse = Math.floorMod((long) Math.pow(a, φM - 1), M);
+        return cipher.chars()
+                .filter(Character::isLetterOrDigit)
+                .map(c -> Character.isDigit(c) ? c : Math.floorMod(inverse * (c - ASCII_A - b), M) + ASCII_A)
+                .mapToObj(Character::toString)
+                .collect(Collectors.joining());
     }
 }
