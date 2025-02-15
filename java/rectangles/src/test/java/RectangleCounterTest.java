fb3532c5928e356f9da6d33dbd480aaef619b49d
Irfan Salim <irfansalim77@gmail.com>
2025-02-15 23:17:30 +0530

    Add  M java/rectangles/src/test/java/RectangleCounterTest.java

--- java/rectangles/src/test/java/RectangleCounterTest.java
+++ java/rectangles/src/test/java/RectangleCounterTest.java
@@ -15,171 +15,158 @@ public class RectangleCounterTest {
 
     @Test
     public void testInputWithNoRowsContainsNoRectangles() {
-        String[] inputGrid = new String[]{};
+        String[] inputGrid = new String[] {};
 
         assertThat(rectangleCounter.countRectangles(inputGrid)).isEqualTo(0);
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void testInputWithNoColumnsContainsNoRectangles() {
-        String[] inputGrid = new String[]{""};
+        String[] inputGrid = new String[] { "" };
 
         assertThat(rectangleCounter.countRectangles(inputGrid)).isEqualTo(0);
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void testNonTrivialInputWithNoRectangles() {
-        String[] inputGrid = new String[]{" "};
+        String[] inputGrid = new String[] { " " };
 
         assertThat(rectangleCounter.countRectangles(inputGrid)).isEqualTo(0);
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void testInputWithOneRectangle() {
-        String[] inputGrid = new String[]{
-            "+-+",
-            "| |",
-            "+-+"
+        String[] inputGrid = new String[] {
+                "+-+",
+                "| |",
+                "+-+"
         };
 
         assertThat(rectangleCounter.countRectangles(inputGrid)).isEqualTo(1);
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void testInputWithTwoRectanglesWithoutSharedEdges() {
-        String[] inputGrid = new String[]{
-            "  +-+",
-            "  | |",
-            "+-+-+",
-            "| |  ",
-            "+-+  "
+        String[] inputGrid = new String[] {
+                "  +-+",
+                "  | |",
+                "+-+-+",
+                "| |  ",
+                "+-+  "
         };
 
         assertThat(rectangleCounter.countRectangles(inputGrid)).isEqualTo(2);
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void testInputWithFiveRectanglesWithSharedEdges() {
-        String[] inputGrid = new String[]{
-            "  +-+",
-            "  | |",
-            "+-+-+",
-            "| | |",
-            "+-+-+"
+        String[] inputGrid = new String[] {
+                "  +-+",
+                "  | |",
+                "+-+-+",
+                "| | |",
+                "+-+-+"
         };
 
         assertThat(rectangleCounter.countRectangles(inputGrid)).isEqualTo(5);
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void testThatRectangleOfHeightOneIsCounted() {
-        String[] inputGrid = new String[]{
-            "+--+",
-            "+--+"
+        String[] inputGrid = new String[] {
+                "+--+",
+                "+--+"
         };
 
         assertThat(rectangleCounter.countRectangles(inputGrid)).isEqualTo(1);
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void testThatRectangleOfWidthOneIsCounted() {
-        String[] inputGrid = new String[]{
-            "++",
-            "||",
-            "++"
+        String[] inputGrid = new String[] {
+                "++",
+                "||",
+                "++"
         };
 
         assertThat(rectangleCounter.countRectangles(inputGrid)).isEqualTo(1);
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void testThatOneByOneSquareIsCounted() {
-        String[] inputGrid = new String[]{
-            "++",
-            "++"
+        String[] inputGrid = new String[] {
+                "++",
+                "++"
         };
 
         assertThat(rectangleCounter.countRectangles(inputGrid)).isEqualTo(1);
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void testThatIncompleteRectanglesAreNotCounted() {
-        String[] inputGrid = new String[]{
-            "  +-+",
-            "    |",
-            "+-+-+",
-            "| | -",
-            "+-+-+"
+        String[] inputGrid = new String[] {
+                "  +-+",
+                "    |",
+                "+-+-+",
+                "| | -",
+                "+-+-+"
         };
 
         assertThat(rectangleCounter.countRectangles(inputGrid)).isEqualTo(1);
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void testThatRectanglesOfDifferentSizesAreAllCounted() {
-        String[] inputGrid = new String[]{
-            "+------+----+",
-            "|      |    |",
-            "+---+--+    |",
-            "|   |       |",
-            "+---+-------+"
+        String[] inputGrid = new String[] {
+                "+------+----+",
+                "|      |    |",
+                "+---+--+    |",
+                "|   |       |",
+                "+---+-------+"
         };
 
         assertThat(rectangleCounter.countRectangles(inputGrid)).isEqualTo(3);
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void testThatIntersectionsWithoutCornerCharacterDoNotCountAsRectangleCorners() {
-        String[] inputGrid = new String[]{
-            "+------+----+",
-            "|      |    |",
-            "+------+    |",
-            "|   |       |",
-            "+---+-------+"
+        String[] inputGrid = new String[] {
+                "+------+----+",
+                "|      |    |",
+                "+------+    |",
+                "|   |       |",
+                "+---+-------+"
         };
 
         assertThat(rectangleCounter.countRectangles(inputGrid)).isEqualTo(2);
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void testLargeInputWithManyRectangles() {
-        String[] inputGrid = new String[]{
-            "+---+--+----+",
-            "|   +--+----+",
-            "+---+--+    |",
-            "|   +--+----+",
-            "+---+--+--+-+",
-            "+---+--+--+-+",
-            "+------+  | |",
-            "          +-+"
+        String[] inputGrid = new String[] {
+                "+---+--+----+",
+                "|   +--+----+",
+                "+---+--+    |",
+                "|   +--+----+",
+                "+---+--+--+-+",
+                "+---+--+--+-+",
+                "+------+  | |",
+                "          +-+"
         };
 
         assertThat(rectangleCounter.countRectangles(inputGrid)).isEqualTo(60);
     }
-    
-    @Disabled("Remove to run test")
+
     @Test
     public void testRectanglesMustHaveFourSides() {
-        String[] inputGrid = new String[]{
-            "+-+ +-+",
-            "| | | |",
-            "+-+-+-+",
-            "  | |  ",
-            "+-+-+-+",
-            "| | | |",
-            "+-+ +-+"
+        String[] inputGrid = new String[] {
+                "+-+ +-+",
+                "| | | |",
+                "+-+-+-+",
+                "  | |  ",
+                "+-+-+-+",
+                "| | | |",
+                "+-+ +-+"
         };
 
         assertThat(rectangleCounter.countRectangles(inputGrid)).isEqualTo(5);
