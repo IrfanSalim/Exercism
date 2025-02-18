c3cea258b8a9e6af209551604bc2c3fc67ab856d
Irfan Salim <irfansalim77@gmail.com>
2025-02-19 01:45:21 +0530

    Add  M java/circular-buffer/src/main/java/CircularBuffer.java

--- java/circular-buffer/src/main/java/CircularBuffer.java
+++ java/circular-buffer/src/main/java/CircularBuffer.java
@@ -1,36 +1,72 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Optional;
+// import java.util.ArrayDeque;
+// import java.util.Deque;
+// import java.util.Optional;
 
-public class CircularBuffer<E> {
-    private final Deque<E> deque = new ArrayDeque<>();
-    private final int size;
+// public class CircularBuffer<E> {
+//     private final Deque<E> deque = new ArrayDeque<>();
+//     private final int size;
 
-    public CircularBuffer(int size) {
-        this.size = size;
+//     public CircularBuffer(int size) {
+//         this.size = size;
+//     }
+
+//     public E read() throws BufferIOException {
+//         E first = deque.pollFirst();
+//         return Optional.ofNullable(first)
+//                 .orElseThrow(() -> new BufferIOException("Tried to read from empty buffer"));
+//     }
+
+//     public void write(E element) throws BufferIOException {
+//         if (deque.size() == size)
+//             throw new BufferIOException("Tried to write to full buffer");
+
+//         deque.addLast(element);
+//     }
+
+//     public void clear() {
+//         deque.clear();
+//     }
+
+//     public void overwrite(E element) {
+//         if (deque.size() == size)
+//             deque.removeFirst();
+
+//         deque.addLast(element);
+//     }
+// }
+
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
+class CircularBuffer<T> {
+
+    private final Queue<T> buffer;
+
+    CircularBuffer(final int size) {
+        buffer = new ArrayBlockingQueue<>(size);
     }
 
-    public E read() throws BufferIOException {
-        E first = deque.pollFirst();
-        return Optional.ofNullable(first)
-                .orElseThrow(() -> new BufferIOException("Tried to read from empty buffer"));
+    T read() throws BufferIOException {
+        if (buffer.isEmpty()) {
+            throw new BufferIOException("Tried to read from empty buffer");
+        }
+        return buffer.poll();
     }
 
-    public void write(E element) throws BufferIOException {
-        if (deque.size() == size)
+    void write(T data) throws BufferIOException {
+        if (!buffer.offer(data)) {
             throw new BufferIOException("Tried to write to full buffer");
-
-        deque.addLast(element);
+        }
     }
 
-    public void clear() {
-        deque.clear();
+    void overwrite(T data) {
+        while (!buffer.offer(data)) {
+            buffer.poll();
+        }
     }
 
-    public void overwrite(E element) {
-        if (deque.size() == size)
-            deque.removeFirst();
-
-        deque.addLast(element);
+    void clear() {
+        buffer.clear();
     }
+
 }
