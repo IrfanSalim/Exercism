4201e08cf416952050af646425ff2145c74f4b95
Irfan Salim <irfansalim77@gmail.com>
2025-03-02 12:34:32 +0530

    Add  M java/sgf-parsing/src/test/java/SgfParsingTest.java

--- java/sgf-parsing/src/test/java/SgfParsingTest.java
+++ java/sgf-parsing/src/test/java/SgfParsingTest.java
@@ -17,7 +17,6 @@ public class SgfParsingTest {
     }
 
     @Test
-    @Disabled("Remove to run test")
     public void treeWithNoNodes() {
         String input = "()";
         assertThatExceptionOfType(SgfParsingException.class)
@@ -26,7 +25,6 @@ public class SgfParsingTest {
     }
 
     @Test
-    @Disabled("Remove to run test")
     public void nodeWithoutTree() {
         String input = ";";
         assertThatExceptionOfType(SgfParsingException.class).as("tree missing")
@@ -34,7 +32,6 @@ public class SgfParsingTest {
     }
 
     @Test
-    @Disabled("Remove to run test")
     public void nodeWithoutProperties() throws SgfParsingException {
         String input = "(;)";
         SgfNode expected = new SgfNode();
@@ -43,7 +40,6 @@ public class SgfParsingTest {
     }
 
     @Test
-    @Disabled("Remove to run test")
     public void singleNodeTree() throws SgfParsingException {
         String input = "(;A[B])";
         SgfNode expected = new SgfNode(Map.of("A", List.of("B")));
@@ -52,7 +48,6 @@ public class SgfParsingTest {
     }
 
     @Test
-    @Disabled("Remove to run test")
     public void multipleProperties() throws SgfParsingException {
         String input = "(;A[b]C[d])";
         SgfNode expected = new SgfNode(Map.of("A", List.of("b"),
@@ -62,7 +57,6 @@ public class SgfParsingTest {
     }
 
     @Test
-    @Disabled("Remove to run test")
     public void propertiesWithoutDelimiter() {
         String input = "(;A)";
         assertThatExceptionOfType(SgfParsingException.class).as("properties without delimiter")
@@ -70,7 +64,6 @@ public class SgfParsingTest {
     }
 
     @Test
-    @Disabled("Remove to run test")
     public void allLowercaseProperty() {
         String input = "(;a[b])";
         assertThatExceptionOfType(SgfParsingException.class).as("property must be in uppercase")
@@ -78,7 +71,6 @@ public class SgfParsingTest {
     }
 
     @Test
-    @Disabled("Remove to run test")
     public void upperAndLowercaseProperty() {
         String input = "(;Aa[b])";
         assertThatExceptionOfType(SgfParsingException.class).as("property must be in uppercase")
@@ -86,32 +78,27 @@ public class SgfParsingTest {
     }
 
     @Test
-    @Disabled("Remove to run test")
     public void twoNodes() throws SgfParsingException {
         String input = "(;A[B];B[C])";
         SgfNode expected = new SgfNode(Map.of("A", List.of("B")),
                 List.of(
-                        new SgfNode(Map.of("B", List.of("C")))
-                ));
+                        new SgfNode(Map.of("B", List.of("C")))));
         SgfNode actual = new SgfParsing().parse(input);
         assertThat(actual).isEqualTo(expected);
     }
 
     @Test
-    @Disabled("Remove to run test")
     public void twoChildTrees() throws SgfParsingException {
         String input = "(;A[B](;B[C])(;C[D]))";
         SgfNode expected = new SgfNode(Map.of("A", List.of("B")),
                 List.of(
                         new SgfNode(Map.of("B", List.of("C"))),
-                        new SgfNode(Map.of("C", List.of("D")))
-                ));
+                        new SgfNode(Map.of("C", List.of("D")))));
         SgfNode actual = new SgfParsing().parse(input);
         assertThat(actual).isEqualTo(expected);
     }
 
     @Test
-    @Disabled("Remove to run test")
     public void multiplePropertyValues() throws SgfParsingException {
         String input = "(;A[b][c][d])";
         SgfNode expected = new SgfNode(Map.of("A", List.of("b", "c", "d")));
@@ -120,7 +107,6 @@ public class SgfParsingTest {
     }
 
     @Test
-    @Disabled("Remove to run test")
     public void withinPropertyValueWhitespace() throws SgfParsingException {
         String input = "(;A[hello\t\tworld])";
         SgfNode expected = new SgfNode(Map.of("A", List.of("hello  world")));
@@ -129,7 +115,6 @@ public class SgfParsingTest {
     }
 
     @Test
-    @Disabled("Remove to run test")
     public void withinPropertyValueNewline() throws SgfParsingException {
         String input = "(;A[hello\n\nworld])";
         SgfNode expected = new SgfNode(Map.of("A", List.of("hello\n\nworld")));
@@ -138,7 +123,6 @@ public class SgfParsingTest {
     }
 
     @Test
-    @Disabled("Remove to run test")
     public void escapedClosingBracket() throws SgfParsingException {
         String input = "(;A[\\]])";
         SgfNode expected = new SgfNode(Map.of("A", List.of("]")));
@@ -147,7 +131,6 @@ public class SgfParsingTest {
     }
 
     @Test
-    @Disabled("Remove to run test")
     public void escapedBacklash() throws SgfParsingException {
         String input = "(;A[\\\\])";
         SgfNode expected = new SgfNode(Map.of("A", List.of("\\")));
@@ -156,55 +139,47 @@ public class SgfParsingTest {
     }
 
     @Test
-    @Disabled("Remove to run test")
     public void openingBracketNeedNotToBeEscaped() throws SgfParsingException {
         String input = "(;A[x[y\\]z][foo]B[bar];C[baz])";
         SgfNode expected = new SgfNode(Map.of("A", List.of("x[y]z", "foo"),
                 "B", List.of("bar")),
                 List.of(
-                        new SgfNode(Map.of("C", List.of("baz")))
-                ));
+                        new SgfNode(Map.of("C", List.of("baz")))));
         SgfNode actual = new SgfParsing().parse(input);
         assertThat(actual).isEqualTo(expected);
     }
 
     @Test
-    @Disabled("Remove to run test")
     public void semicolonNeedNotToBeEscaped() throws SgfParsingException {
         String input = "(;A[a;b][foo]B[bar];C[baz])";
         SgfNode expected = new SgfNode(Map.of("A", List.of("a;b", "foo"),
                 "B", List.of("bar")),
                 List.of(
-                        new SgfNode(Map.of("C", List.of("baz")))
-                ));
+                        new SgfNode(Map.of("C", List.of("baz")))));
         SgfNode actual = new SgfParsing().parse(input);
         assertThat(actual).isEqualTo(expected);
     }
 
     @Test
-    @Disabled("Remove to run test")
     public void paranthesesNeedNotToBeEscaped() throws SgfParsingException {
         String input = "(;A[x(y)z][foo]B[bar];C[baz])";
         SgfNode expected = new SgfNode(Map.of("A", List.of("x(y)z", "foo"),
                 "B", List.of("bar")),
                 List.of(
-                        new SgfNode(Map.of("C", List.of("baz")))
-                ));
+                        new SgfNode(Map.of("C", List.of("baz")))));
         SgfNode actual = new SgfParsing().parse(input);
         assertThat(actual).isEqualTo(expected);
     }
 
     @Test
-    @Disabled("Remove to run test")
     public void escapedTab() throws SgfParsingException {
         String input = "(;A[hello\\\tworld])";
         SgfNode expected = new SgfNode(Map.of("A", List.of("hello world")));
         SgfNode actual = new SgfParsing().parse(input);
         assertThat(actual).isEqualTo(expected);
     }
-    
+
     @Test
-    @Disabled("Remove to run test")
     public void escapedNewline() throws SgfParsingException {
         String input = "(;A[hello\\\nworld])";
         SgfNode expected = new SgfNode(Map.of("A", List.of("helloworld")));
@@ -212,9 +187,7 @@ public class SgfParsingTest {
         assertThat(actual).isEqualTo(expected);
     }
 
-    
     @Test
-    @Disabled("Remove to run test")
     public void escapedTAndN() throws SgfParsingException {
         String input = "(;A[\\t = t and \\n = n])";
         SgfNode expected = new SgfNode(Map.of("A", List.of("t = t and n = n")));
@@ -222,9 +195,7 @@ public class SgfParsingTest {
         assertThat(actual).isEqualTo(expected);
     }
 
-    
     @Test
-    @Disabled("Remove to run test")
     public void mixOfEscapedCharactersAndWhitespaces() throws SgfParsingException {
         String input = "(;A[\\]b\nc\\\nd\t\te\\\\ \\\n\\]])";
         SgfNode expected = new SgfNode(Map.of("A", List.of("]b\ncd  e\\ ]")));
