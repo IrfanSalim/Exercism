ceb0e69c1fe9b1007cc6f251a2142d1bd293cd91
Irfan Salim <irfansalim77@gmail.com>
2025-03-09 15:32:26 +0530

    Add  M java/react/gradlew  M java/react/src/main/java/React.java

--- java/react/src/main/java/React.java
+++ java/react/src/main/java/React.java
@@ -1,36 +1,74 @@
-import java.util.List;
+import java.util.*;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class React {
 
+    public static <T> InputCell<T> inputCell(final T initialValue) {
+        final var inputCell = new InputCell<T>();
+        inputCell.setValue(initialValue);
+        return inputCell;
+    }
+
+    public static <T> ComputeCell<T> computeCell(Function<List<T>, T> computation, List<Cell<T>> inputCells) {
+        final var computeCell = new ComputeCell<>(inputCells, computation);
+        inputCells.forEach(c -> c.addListener(computeCell));
+        return computeCell;
+    }
+
     public static class Cell<T> {
+        protected final Set<ComputeCell<T>> listeners = new HashSet<>();
+        protected T value;
+
         public T getValue() {
-            throw new UnsupportedOperationException("Please implement the Cell.getValue() method");
+            return value;
+        }
+
+        public void addListener(ComputeCell<T> listener) {
+            this.listeners.add(listener);
         }
     }
 
     public static class InputCell<T> extends Cell<T> {
-        public void setValue(T newValue) {
-            throw new UnsupportedOperationException("Please implement the InputCell.setValue() method");
+        public void setValue(final T newValue) {
+            if (newValue.equals(this.getValue()))
+                return;
+            this.value = newValue;
+            listeners.forEach(ComputeCell::update);
         }
     }
 
     public static class ComputeCell<T> extends Cell<T> {
+        private final List<Cell<T>> inputCells;
+        private final Function<List<T>, T> computation;
+        private final Set<Consumer<T>> callbacks = new HashSet<>();
+
+        public ComputeCell(List<Cell<T>> inputCells, Function<List<T>, T> computation) {
+            this.inputCells = inputCells;
+            this.computation = computation;
+            this.value = this.getValue();
+        }
+
         public void addCallback(Consumer<T> callback) {
-            throw new UnsupportedOperationException("Please implement the ComputeCell.addCallback() method");
+            callbacks.add(callback);
         }
 
         public void removeCallback(Consumer<T> callback) {
-            throw new UnsupportedOperationException("Please implement the ComputeCell.removeCallback() method");
+            callbacks.remove(callback);
         }
-    }
 
-    public static <T> InputCell<T> inputCell(T initialValue) {
-        throw new UnsupportedOperationException("Please implement the React.inputCell() method");
-    }
+        @Override
+        public T getValue() {
+            return computation.apply(inputCells.stream().map(Cell::getValue).toList());
+        }
 
-    public static <T> ComputeCell<T> computeCell(Function<List<T>, T> function, List<Cell<T>> cells) {
-        throw new UnsupportedOperationException("Please implement the React.computeCell() method");
+        public void update() {
+            final T newValue = this.getValue();
+            if (this.value.equals(newValue))
+                return;
+            this.value = newValue;
+            listeners.forEach(ComputeCell::update);
+            callbacks.forEach(tConsumer -> tConsumer.accept(newValue));
+        }
     }
 }
