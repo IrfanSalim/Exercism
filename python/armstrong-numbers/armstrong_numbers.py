1c22d003098b72fed7f4b51a4ac1fa6ce10f72b4
Irfan Salim <irfansalim77@gmail.com>
2025-04-27 20:10:35 +0530

    Add  M python/armstrong-numbers/armstrong_numbers.py

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,7 @@
 def is_armstrong_number(number):
-    pass
+    digits = list(str(number))
+    power = len(digits)
+    digit_sum = 0
+    for num in digits:
+        digit_sum += int(num)**power
+    return digit_sum == number
