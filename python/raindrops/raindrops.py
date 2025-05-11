37ce6b762d6b210624449954cc2c52ed6ee8d59c
Irfan Salim <irfansalim77@gmail.com>
2025-05-11 21:24:19 +0530

    Add  M python/raindrops/raindrops.py

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -2,3 +2,7 @@ def convert(number):
 
     return ''.join( v for k,v in { 3: "Pling", 5: "Plang", 7: "Plong" }.items() if number % k == 0 ) or str(number)
 
+# def convert(number):
+#     return (("" if number % 3 else "Pling") +
+#             ("" if number % 5 else "Plang") +
+#             ("" if number % 7 else "Plong")) or str(number)
