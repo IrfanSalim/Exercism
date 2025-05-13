5434fc769c0d60028b17cd7cb825223485191765
Irfan Salim <irfansalim77@gmail.com>
2025-05-13 19:41:56 +0530

    Add  M python/perfect-numbers/perfect_numbers.py

--- python/perfect-numbers/perfect_numbers.py
+++ python/perfect-numbers/perfect_numbers.py
@@ -4,4 +4,12 @@ def classify(number):
     :param number: int a positive integer
     :return: str the classification of the input integer
     """
-    pass
+    if number < 1:
+        raise ValueError("Classification is only possible for positive integers.")
+    total = sum(i for i in range(1, number//2+1) if not number % i)
+    if total == number:
+        return "perfect"
+    elif total > number:
+        return "abundant"
+    else:
+        return "deficient"
