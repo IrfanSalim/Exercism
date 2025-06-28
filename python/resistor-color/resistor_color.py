db0a67636893fa24950b538c58af7b01e735837b
Irfan Salim <irfansalim77@gmail.com>
2025-06-28 21:12:59 +0530

    Add  M python/resistor-color/resistor_color.py

--- python/resistor-color/resistor_color.py
+++ python/resistor-color/resistor_color.py
@@ -1,6 +1,7 @@
-def color_code(color):
-    pass
+COLORS = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
 
+def color_code(color):
+    return COLORS.index(color)
 
 def colors():
-    pass
+    return COLORS
