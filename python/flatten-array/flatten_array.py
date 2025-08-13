82134c1bdb5cee556db631f7356886daf64edbf1
Irfan Salim <irfansalim77@gmail.com>
2025-08-13 12:10:15 +0530

    Add  M python/flatten-array/flatten_array.py

--- python/flatten-array/flatten_array.py
+++ python/flatten-array/flatten_array.py
@@ -1,2 +1,8 @@
 def flatten(iterable):
-    pass
+    flat = []
+    for item in iterable:
+        if isinstance(item, list):
+            flat.extend(flatten(item))
+        elif item is not None:
+            flat.append(item)
+    return(flat)
