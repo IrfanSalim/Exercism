571f3415b8e04a101664c1d6a0447a2862681bfb
Irfan Salim <irfansalim77@gmail.com>
2025-08-16 23:43:09 +0530

    Add  M python/difference-of-squares/difference_of_squares.py

--- python/difference-of-squares/difference_of_squares.py
+++ python/difference-of-squares/difference_of_squares.py
@@ -1,10 +1,8 @@
-def square_of_sum(number):
-    pass
+def sum_of_squares(n: int) -> int:
+    return sum(map(lambda x: x ** 2, range(1, n + 1)))
 
+def square_of_sum(n: int) -> int:
+    return sum(range(1, n + 1)) ** 2
 
-def sum_of_squares(number):
-    pass
-
-
-def difference_of_squares(number):
-    pass
+def difference_of_squares(n: int) -> int:
+    return square_of_sum(n) - sum_of_squares(n)
