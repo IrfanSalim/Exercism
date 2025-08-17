31a06299fde5d3f4d56f46101a11093080102c2f
Irfan Salim <irfansalim77@gmail.com>
2025-08-18 00:31:00 +0530

    Add  M python/matching-brackets/matching_brackets.py

--- python/matching-brackets/matching_brackets.py
+++ python/matching-brackets/matching_brackets.py
@@ -1,2 +1,6 @@
-def is_paired(input_string):
-    pass
+import re
+def is_paired(stg):
+    brackets, has_pair = re.sub(r"[^{}[\]()]", "", stg), 1
+    while has_pair:
+        brackets, has_pair = re.subn(r"{}|\[]|\(\)", "", brackets)
+    return not brackets
