8625fc6780a89d7a400a0285aef26b5d1b3373cc
Irfan Salim <irfansalim77@gmail.com>
2025-09-06 00:02:45 +0530

    Add  M python/atbash-cipher/atbash_cipher.py

--- python/atbash-cipher/atbash_cipher.py
+++ python/atbash-cipher/atbash_cipher.py
@@ -1,6 +1,13 @@
+from string import ascii_lowercase as lower, punctuation as punct
+
+table = str.maketrans(lower, lower[::-1], ' '+punct)
+csize = 5
+
+
 def encode(plain_text):
-    pass
+    out = plain_text.lower().translate(table)
+    return ' '.join([out[i:i+5] for i in range(0, len(out), 5)])
 
 
 def decode(ciphered_text):
-    pass
+    return ciphered_text.translate(table)
