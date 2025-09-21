33dfb984d1642d60a631e1413f2759b7a492378c
Irfan Salim <irfansalim77@gmail.com>
2025-09-21 23:57:28 +0530

    Add  M python/nth-prime/nth_prime.py

--- python/nth-prime/nth_prime.py
+++ python/nth-prime/nth_prime.py
@@ -1,2 +1,13 @@
 def prime(number):
-    pass
+
+    if number < 1:
+        raise ValueError('there is no zeroth prime')
+    
+    n = 2
+    primes = [2]
+    while len(primes) < number:
+        n += 1
+        if all(n % p > 0 for p in primes):
+            primes.append(n)
+    
+    return primes[number - 1]
