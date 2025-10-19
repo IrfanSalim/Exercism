5d1878a8aa8d5d2e333aa6a1f9fd766cdcef6914
Irfan Salim <irfansalim77@gmail.com>
2025-10-19 23:55:47 +0530

    Add  M python/run-length-encoding/run_length_encoding.py

--- python/run-length-encoding/run_length_encoding.py
+++ python/run-length-encoding/run_length_encoding.py
@@ -1,6 +1,11 @@
-def decode(string):
-    pass
+from re import findall
 
+def encode(text):
+    return ''.join(
+        "%d%s" % (len(letters), letter) if len(letters) > 1 else letter
+        for letters, letter in findall(r'((.)\2*)', text))
 
-def encode(string):
-    pass
+def decode(compressed):
+    return ''.join(
+        int(count) * letter if count else letter
+        for count, letter in findall('(\d*)(.)', compressed))
