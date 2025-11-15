a32c6f9e69eb19817b30222fac6b8d79f96fcc3d
Irfan Salim <irfansalim77@gmail.com>
2025-11-15 23:55:29 +0530

    Add  M python/crypto-square/crypto_square.py

--- python/crypto-square/crypto_square.py
+++ python/crypto-square/crypto_square.py
@@ -1,2 +1,11 @@
-def cipher_text(plain_text):
-    pass
+def cipher_text(plain_text: str) -> str:
+    normalized = ''.join(char for char in plain_text if char.isalnum()).lower()
+    norm_len = len(normalized)
+
+    cols = rows = round(norm_len ** 0.5)
+    if cols ** 2 < norm_len:
+        cols += 1
+
+    normalized += ' ' * (cols * rows - norm_len)
+
+    return ' '.join(normalized[i::cols] for i in range(cols))
