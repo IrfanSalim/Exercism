695e38638c6232788852b818de91ff9e9d0ca6dc
Irfan Salim <irfansalim77@gmail.com>
2025-12-06 23:46:05 +0530

    Add  M python/killer-sudoku-helper/killer_sudoku_helper.py

--- python/killer-sudoku-helper/killer_sudoku_helper.py
+++ python/killer-sudoku-helper/killer_sudoku_helper.py
@@ -1,2 +1,5 @@
+import itertools
+
 def combinations(target, size, exclude):
-    pass
+    available_numbers = list(set([1, 2, 3, 4, 5, 6, 7, 8, 9]) - set(exclude))
+    return [list(combination) for combination in itertools.combinations(available_numbers, size) if sum(combination) == target]
